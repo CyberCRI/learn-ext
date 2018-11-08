4f691b5fb72ae19da0a237a8b706ff4cd0796e43
Prashant Sinha <prashant+git@noop.pw>
2018-11-08 14:37:32 +0100

    improve popout component organisation and split the modules

--- src/components/popout.js
+++ src/components/popout.js
@@ -1,9 +1,97 @@
 import React, { Component } from 'react'
+import { List } from 'immutable'
+import { hot } from 'react-hot-loader'
 
-import { Button, Popover, NonIdealState, Tag } from '@blueprintjs/core'
+import { Button, Popover, NonIdealState, Tag, MenuItem, FormGroup, Intent } from '@blueprintjs/core'
+import { MultiSelect } from '@blueprintjs/select'
+
 
 import { get_concepts } from './remote'
 import './popout.sass'
+
+
+class ConceptsField extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      keywords: List(),
+      concepts: List(),
+      selected: List(),
+    }
+  }
+
+  componentDidMount () {
+    get_concepts().then((data) => {
+      const keywords = List(data.keywords)
+      const concepts = List(data.concepts)
+      this.setState({
+        keywords,
+        concepts,
+        selected: concepts.slice(1),
+      })
+
+    })
+  }
+
+  isSelected (item) {
+    return this.state.selected.indexOf(item) !== -1
+  }
+
+  renderTag (item) {
+    return (
+      <Tag
+        className='bp3-skeleton'
+        key={item.label}
+        intent={Intent.PRIMARY}
+        interactive
+        minimal>
+        {item.label}
+      </Tag>
+    )
+  }
+
+  renderOption (item) {
+    const active = this.state.selected.find((x) => x.label === item.label)
+    return (
+      <MenuItem
+        key={item.label}
+        text={item.label}
+        shouldDismissPopover={false}
+        icon={active ? 'tick' : 'blank'}
+      />)
+  }
+
+  render () {
+    return (
+      <FormGroup
+        helperText='Add or update tags'
+        label='Keywords'>
+
+        <MultiSelect
+          tagInputProps={{
+            // leftIcon: 'tag',
+            fill: true,
+            large: true,
+            rightElement: <Button icon='plus' minimal/>,
+            tagProps: {
+              interactive: true,
+              minimal: true,
+            }
+          }}
+          items={this.state.concepts.toJS()}
+          selectedItems={this.state.selected}
+          tagRenderer={(x) => x.label}
+          onItemSelect={(x) => {
+            const selected = [...this.state.selected, x]
+            this.setState({ selected })
+          }}
+          itemRenderer={(x) => this.renderOption(x)}
+          />
+      </FormGroup>
+    )
+  }
+}
 
 
 class Popout extends Component {
@@ -11,27 +99,17 @@ class Popout extends Component {
     super(props)
     this.state = {
       ...props.value,
-      keywords: [],
-      concepts: [],
     }
-  }
-
-  componentDidMount () {
-    get_concepts().then((data) => {
-      this.setState(data)
-    })
   }
 
   render () {
     return (
       <div className='ext--root'>
-        <Popover>
+        <Popover position='left-top' defaultIsOpen>
           <Button icon='book' />
           <div className='ext popover'>
-            <div className='tags'>
-              {this.state.concepts.map((x) => <Tag key={x.label}>{x.label}</Tag>)}
-            </div>
-            <NonIdealState icon='code-block' action={<Button icon='refresh'/>} />
+            <h2>iLearn</h2>
+            <ConceptsField />
           </div>
         </Popover>
       </div>
@@ -39,4 +117,4 @@ class Popout extends Component {
   }
 }
 
-export default Popout
+export default hot(module)(Popout)
--- src/components/remote.js
+++ src/components/remote.js
@@ -1,11 +1,11 @@
 import { request, getCanonicalUrl } from './utils'
 
+
 const ENDPOINT = 'https://api-ilearn.zen.noop.pw/concepts'
 
 
 const get_concepts = () => {
   const page_url = getCanonicalUrl()
-  console.log(page_url)
 
   return request({
     url: ENDPOINT,
