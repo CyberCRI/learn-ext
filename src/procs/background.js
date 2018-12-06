8363c546acb3c8e309782c58c9235f54d71a85ec
Prashant Sinha <prashant+git@noop.pw>
2018-12-06 16:43:15 +0100

    Add background script for extension process

---
+++ src/procs/background.js
@@ -0,0 +1,27 @@
+browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
+  if (tab.url.includes('en.wikipedia')) {
+    browser.pageAction
+      .show(tab.id)
+      .then(() => {
+        browser.pageAction.setIcon({ tabId: tab.id, path: "assets/icon16.png" })
+    })
+  } else {
+    browser.pageAction.hide(tab.id)
+  }
+  browser.pageAction.setTitle({ tabId: tab.id, title: 'iLearn Button' })
+})
+
+browser.pageAction.onClicked.addListener((e) => {
+  browser.pageAction
+    .setIcon({ tabId: e.id, path: "highlights.svg" })
+
+  browser.tabs.sendMessage(e.id, { activate: true })
+  console.log(e)
+
+  browser.notifications.create({
+    type: 'basic',
+    iconUrl: browser.extension.getURL(e.favIconUrl),
+    title: 'Added to iLearn',
+    message: `Added this page ${e.title} <${e.id}> to ilearn. (not really)`,
+  })
+})
