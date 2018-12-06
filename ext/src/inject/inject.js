6a73a4d15a0f06106906924ed6737ce44df29762
Prashant Sinha <prashant+git@noop.pw>
2018-12-06 10:59:47 +0100

    Remove useless boilerplate codes from extension and intialise a cleaner manifest file

--- ext/src/inject/inject.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $.get(chrome.extension.getURL('src/inject/toolbar.html'), (data) => {
-    $(data).appendTo('body')
-
-    $('#np-toggle').click((e) => {
-      $('.np-ext .tags').toggleClass('open')
-    })
-  })
-})
