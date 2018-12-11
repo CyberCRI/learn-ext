40a3c7279fad61d4d3e652233e668f0eee7b8a78
Prashant Sinha <prashant+git@noop.pw>
2018-12-11 14:27:28 +0100

    Add preferences overrides to web-ext

--- web-ext-config.js
+++ web-ext-config.js
@@ -1,6 +1,31 @@
-const { PackageEnv, abspath } = require('./package.config.js')
+// Configuration for web-ext cli.
+// References at https://github.com/mozilla/web-ext
+const { PackageEnv } = require('./package.config.js')
+const _ = require('lodash')
 
-console.log(PackageEnv.vars)
+console.log('[!] Using Environment:', PackageEnv.vars)
+
+// Collect the preferences overriden above to the correct format.
+// Essentially, these parameters are added in the `about:config` page. The cli
+// tool expects them to be in format of `key=value`, hence we apply a map to
+// format this object.
+const browser_prefs = _({
+  'lightweightThemes.selectedThemeID': 'firefox-compact-light@mozilla.org',
+  'browser.uidensity': 1,
+  'browser.EULA.override': true,
+  'devtools.dom.enabled': true,
+  'devtools.editor.keymap': 'sublime',
+  'devtools.errorconsole.enabled': true,
+  'devtools.theme': 'light',
+  'devtools.toolbox.splitconsoleHeight': 200,
+  'devtools.webconsole.filter.net': true,
+  'devtools.webconsole.filter.netxhr': true,
+  'devtools.browserconsole.filter.net': true,
+  'devtools.browserconsole.filter.netxhr': true,
+  'browser.uitour.enabled': false,
+}).map((value, key) => `${key}=${value}`)
+  .value()
+
 
 module.exports = {
   sourceDir: './ext',
@@ -8,13 +33,9 @@ module.exports = {
   run: {
     firefox: PackageEnv.vars.webext_firefox_version,
     browserConsole: true,
-
-    pref: {
-      // 'browserUidensity': 1,
-    },
-
-    // firefoxProfile: PackageEnv.vars.webext_profile_namespace,
-    startUrl: ['about:debugging', 'https://en.wikipedia.org'],
+    pref: browser_prefs,
+    noReload: false,
+    startUrl: ['https://en.wikipedia.org/wiki/Solar_System', 'https://en.wikipedia.org'],
   },
 
   sign: {
