f953a261ecae33f1e3bf0fdad213b759e184c04a
Prashant Sinha <prashant+git@noop.pw>
2019-01-16 11:37:36 +0100

    Add the dev config for stylesheets which uses 'style-loader'

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,12 +3,29 @@ const merge = require('webpack-merge')
 
 const base_config = require('./webpack.common')
 
+const merge_conf = {
+  'module.rules.use': 'prepend',
+}
 
-module.exports = merge(base_config, {
+module.exports = merge.smartStrategy(merge_conf)(base_config, {
   mode: 'development',
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader' ],
+      },
+      {
+        test: /\.s(c|a)ss$/,
+        exclude: /node_modules/,
+        use: [ 'style-loader' ],
+      },
+    ],
+  },
 
   devtool: 'inline-source-map',
   devServer: {
