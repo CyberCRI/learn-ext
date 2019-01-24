a6338f3b302e3d3e253c4ae4a754a9ae5e27cec4
Prashant Sinha <prashant+git@noop.pw>
2019-01-24 15:41:31 +0100

    Add tabs and empty panels

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -5,11 +5,46 @@ import { renderReactComponent } from '~mixins/utils'
 
 import './_settings.sass'
 
+
+const PanelGeneral = () => (
+  <div>
+  </div>
+)
+
+const PanelAccount = () => (
+  <div>
+  </div>
+)
+
+const PanelPrivacy = () => (
+  <div>
+  </div>
+)
+
+const PanelSupport = () => (
+  <div>
+  </div>
+)
+
+
+const SettingsTabs = (props) => {
+  return (
+    <Tabs large vertical id='pages-settings'>
+      <Tab title='General' id='general' panel={<PanelGeneral/>}/>
+      <Tab title='Account' id='account' panel={<PanelAccount/>}/>
+      <Tab title='Privacy' id='privacy' panel={<PanelPrivacy/>}/>
+      <Divider/>
+      <Tab title='Support' id='support' panel={<PanelSupport/>}/>
+    </Tabs>
+  )
+}
+
 const SettingsContainer = (props) => {
   return (
     <Card elevation={Elevation.THREE}>
       <h2>Settings</h2>
 
+      <SettingsTabs />
     </Card>
   )
 }
