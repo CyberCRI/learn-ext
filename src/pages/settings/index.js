b5bba39603caf8e3408650030c0c5c205a9b0c45
Prashant Sinha <prashant+git@noop.pw>
2019-01-24 15:40:31 +0100

    Add page handler

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -1,5 +1,21 @@
+import React, { Component } from 'react'
+import { Card, Callout, Divider, Intent, Tabs, Tab, Elevation } from '@blueprintjs/core'
+
+import { renderReactComponent } from '~mixins/utils'
+
 import './_settings.sass'
+
+const SettingsContainer = (props) => {
+  return (
+    <Card elevation={Elevation.THREE}>
+      <h2>Settings</h2>
+
+    </Card>
+  )
+}
+
 
 window.addEventListener('load', () => {
   console.info('Init Settings')
+  renderReactComponent('settings', SettingsContainer)
 })
