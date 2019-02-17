783b86a54ab48958a91cff15ae8c639adf1c5f80
Prashant Sinha <prashant+git@noop.pw>
2019-02-17 22:24:46 +0100

    Implement onboarding page

--- src/pages/onboarding/index.js
+++ src/pages/onboarding/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Card, Callout, Divider, Intent, Tabs, Tab, Elevation } from '@blueprintjs/core'
 import { renderReactComponent } from '~mixins/utils'
+import { AccountSelector } from '~components/input/settings'
 
 import './_onboarding.sass'
 
@@ -8,11 +9,11 @@ import './_onboarding.sass'
 const WelcomeCard = () => (
   <Card elevation={Elevation.THREE}>
     <h2>Thanks for using iLearn!</h2>
+
+    <AccountSelector/>
   </Card>
 )
 
-
-window.addEventListener('load', () => {
-  console.info('Init Settings')
+document.addEventListener('apploaded', () => {
   renderReactComponent('onboarding', WelcomeCard)
 })
