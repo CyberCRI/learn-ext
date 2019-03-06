f91941694ebba734404998756bef8397380b5049
Prashant Sinha <prashant+git@noop.pw>
2019-03-06 16:51:04 +0100

    Register and render styled buttons

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,5 +1,10 @@
 import { renderReactComponent } from '~mixins/utils'
 
+import { DownloadLinks, FirefoxLink } from './download-buttons'
+
 import './_landing.sass'
+
+
 document.addEventListener('apploaded', () => {
+  renderReactComponent('download-links', DownloadLinks)
 })
