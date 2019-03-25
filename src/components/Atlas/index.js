eb32be5a369144773f4da281a6460e142dab8b90
Prashant Sinha <prashant+git@noop.pw>
2019-03-25 13:57:33 +0100

    Cleanup Atlas component

--- src/components/Atlas/index.js
+++ src/components/Atlas/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import clsx from 'classnames'
+import _ from 'lodash'
 
 
 class Atlas extends Component {
@@ -9,30 +10,35 @@ class Atlas extends Component {
     this.state = {
       ready: false,
     }
+    this.processPoints = this.processPoints.bind(this)
+    this.draw = this.draw.bind(this)
   }
 
   componentDidMount () {
     this.atlas = new DotAtlas({
       element: this.canvasRef,
       pixelRatio: 2,
-      maxRadiusDivider: 10,
+      maxRadiusDivider: 5,
       mapLightAzimuth: 0.4,
       mapLightIntensity: 0.5,
     })
     this.atlasFx = new DotAtlasEffects(this.atlas)
+    if (this.props.dataPoints.length) {
+      this.draw()
+    }
   }
 
   componentDidUpdate (prevProps, prevState) {
     if (this.props.ready !== prevState.ready) {
       // Ready state changed, so we'll need to refresh the data points and redraw
-      this.drawAtlas()
+      this.draw()
     }
   }
 
   processPoints () {
     // Implement logic for processing the datapoints.
     const red = [200, 0, 0, 255]
-    this.props.dataPoints.map((p, ix) => {
+    return this.props.dataPoints.map((p, ix) => {
       p.elevation = ix === 0 ? 1 : 0.2
       p.marker = 'circle'
       p.markerSize = 0
@@ -45,14 +51,28 @@ class Atlas extends Component {
     this.map.resize()
   }
 
-  drawAtlas () {
-    const { points } = this.props
+  draw () {
+    const points = this.props.dataPoints
     const elevations = { points }
     const dataObject = {
       layers: [ elevations ],
     }
 
     this.atlasFx.rollout(dataObject)
+
+    // let azimuth = 0
+    // setInterval(() => {
+    //   azimuth += 0.1
+    //   this.atlas.set('mapLightAzimuth', azimuth)
+    //   this.atlas.redraw()
+    // }, 200)
+    // setInterval(() => {
+    //   const { x, y } = _.sample(points)
+    //   this.atlas.reset()
+    //   setTimeout(() => {
+    //     this.atlas.centerPoint(x, y, 10, 1000)
+    //   }, 300)
+    // }, 5000)
   }
 
   render () {
@@ -65,57 +85,4 @@ class Atlas extends Component {
   }
 }
 
-function drawCartography (points, container) {
-  // Coordinates of points on the map.
-
-  // Assign additional properties to some of the points.
-  // These could of course be defined directly in the data above,
-  // we use the loop to avoid repetition.
-  const red = [200, 0, 0, 255];
-  points.forEach(function (p, index) {
-    // Elevation of the point on the 0..1 scale.
-    // Zero elevation will not contribute to the height map,
-    // but may still display markers or labels, if defined.
-    p.elevation = index === 0 ? 1 : 0.2;
-
-    // To display a marker, the point must have a non-empty "marker" property.
-    // The property defines shape of the marker: "circle", "square", "triangle".
-    p.marker = "circle";
-
-    // Marker size on the 0..1 scale. Zero marker size will draw markers
-    // of very small sizes, near to 1 pixel. This might be useful for
-    // drawing hundreds of thousands of markers.
-    p.markerSize = 0;
-
-    // Color of the marker. The required format is a 4-element RGBA array,
-    // with each component value in the 0..255 range.
-    // Tip: you can reuse array references for same-colored markers.
-    p.markerColor = red;
-  });
-
-  // Define layers.
-
-  // Currently, the elevations layer is special: this is the only layer that can define
-  // point elevations and labels. Bounding box of the map and hover events are computed
-  // only for points on this layer. The special status is likely to be removed before
-  // the first official release.
-  const elevations = { points: points };
-
-  // Point markers layer, draws point markers (circles in this example) on top
-  // of the elevation layer. The markerSizeMultiplier defines how large in pixels
-  // a marker with markerSize = 1 should be.
-  const markers =    { points: points, type: "marker", markerSizeMultiplier: 10 };
-
-  // Initialize dotAtlas
-  const dotatlas = new DotAtlas({
-    element: container,
-    dataObject: {
-      layers: [
-        elevations, // Currently, elevation layer must always be at index 0
-        markers
-      ]
-    },
-    pixelRatio: window.devicePixelRatio || 1
-  });
-  return dotatlas;
-}
+export default Atlas
