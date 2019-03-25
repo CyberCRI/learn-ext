cd52fce9da8a2b0ee808aa396a8d003f7ce839ec
Prashant Sinha <prashant+git@noop.pw>
2019-03-25 14:27:30 +0100

    Handle standard form submissions on Account info page

--- src/components/input/settings.js
+++ src/components/input/settings.js
@@ -32,7 +32,9 @@ class AccountSelector extends Component {
       })
   }
 
-  didUpdateUserSettings () {
+  didUpdateUserSettings (e) {
+    // Ensure we don't move to other page.
+    e.preventDefault()
     if (this.state.username.length >= 2) {
       // For now, we just don't really bother about validation...
       const payload = {
@@ -48,13 +50,13 @@ class AccountSelector extends Component {
   render () {
     const inputIntent = this.state.username.length >= 2 ? Intent.PRIMARY : Intent.DANGER
     return (
-      <div>
+      <form onSubmit={this.didUpdateUserSettings}>
         <Callout icon='id-number' title='Account' intent={Intent.DEFAULT}>
           <p>Specify the email you want to use iLearn with.</p>
 
           {this.state.signedIn && <p>You are signed in!</p>}
 
-          <ControlGroup>
+          <ControlGroup >
             <InputGroup
               leftIcon='id-number'
               disabled={this.state.loading}
@@ -63,12 +65,14 @@ class AccountSelector extends Component {
               onChange={(e) => this.setState({ username: e.target.value })}/>
             <Button
               text='Save'
+              type='submit'
+              intent={inputIntent}
               loading={this.state.loading}
               icon={<FiIcon.FiSave/>}
               onClick={this.didUpdateUserSettings}/>
           </ControlGroup>
         </Callout>
-      </div>
+      </form>
     )
   }
 }
