c67962175777af5aaee4f8e0dd48406baaa3e9e4
Prashant Sinha <prashant+git@noop.pw>
2019-03-25 13:56:06 +0100

    Complete base landing page layout and styles

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,10 +1,31 @@
 import { renderReactComponent } from '~mixins/utils'
 
-import { DownloadLinks, FirefoxLink } from './download-buttons'
+import { DownloadLinks } from './download-buttons'
+import { BlogCallout } from './cards'
+import Atlas from '~components/Atlas'
+import { request } from '~mixins'
+import InlineSVG from 'svg-inline-react'
 
 import './_landing.sass'
+
+import cri_logo from '~media/logo-cri.white.green.svg'
+import planet from '~media/planets/cri.planets-all.svg'
+import learning from '~media/illustrations/learning.svg'
+import people from '~media/illustrations/people.svg'
 
 
 document.addEventListener('apploaded', () => {
   renderReactComponent('download-links', DownloadLinks)
+
+  renderReactComponent('logo', InlineSVG, { src: cri_logo, element: 'figure' } )
+  renderReactComponent('planets', InlineSVG, { src: planet, element: 'figure' } )
+  renderReactComponent('people', InlineSVG, { src: people, element: 'figure' } )
+  renderReactComponent('learning', InlineSVG, { src: learning, element: 'figure' } )
+
+  renderReactComponent('callout', BlogCallout )
+
+  request({ url: 'https://noop-pub.s3.amazonaws.com/opt/dotatlas_tmp.json' })
+    .then((points) => {
+      renderReactComponent('atlas', Atlas, { dataPoints: points })
+    })
 })
