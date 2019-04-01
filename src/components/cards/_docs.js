cd342bb7e9d80af25d96bb89a102e4f1f1ec4a24
Prashant Sinha <prashant+git@noop.pw>
2019-04-01 17:27:11 +0200

    Create a documentation component for Wiki Cards

---
+++ src/components/cards/_docs.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Popover, PopoverInteractionKind, Position } from '@blueprintjs/core'
+
+import { WikiCard } from './wiki-concept'
+
+
+const WikiPopoverCard = (props) => (
+  <Popover
+    content={<WikiCard title={props.label}/>}
+    target={<span>{props.label}</span>}
+    interactionKind={PopoverInteractionKind.HOVER}
+    hoverCloseDelay={500}
+    hoverOpenDelay={200}
+    inheritDarkTheme={false}
+    isOpen={true}
+    usePortal={false}
+    enforceFocus={false}
+    position={Position.RIGHT}/>
+)
+
+export { WikiPopoverCard }
