3fb5c6596a562aca7b3b8db62da513cc01993c5f
Prashant Sinha <prashant+git@noop.pw>
2019-04-01 17:21:36 +0200

    Implement WikiCard component

--- src/components/cards/wiki-concept.js
+++ src/components/cards/wiki-concept.js
@@ -17,3 +17,50 @@ const SkeletonCard = () => (
   </Card>
 )
 
+
+const WikiCard = (props) => {
+  const [ pageInfo, setPageInfo ] = useState(null)
+
+  if (!pageInfo) {
+    Wiki.summary(props.title)
+      .then((data) => {
+        setPageInfo(data)
+      })
+  }
+
+  if (!pageInfo) {
+    return <SkeletonCard />
+  }
+
+  const contentUrl = pageInfo.content_urls.desktop.page
+
+  return (
+    <Card interactive elevation={Elevation.TWO} className={clsx('info-card', 'bp3-dark')}>
+      <div className='content'>
+        <h3 className='title'>
+          {pageInfo.title}
+          <cite>From Wikipedia</cite>
+        </h3>
+        <p className='summary'>{pageInfo.extract}</p>
+      </div>
+      {pageInfo.thumbnail && <img className='thumbnail' src={pageInfo.thumbnail.source}/>}
+      <div className='tools'>
+        <ButtonGroup fill minimal>
+          <AnchorButton
+            icon='send-to-map'
+            text='Locate in Map'
+            href={contentUrl}
+            target='_blank'/>
+          <AnchorButton
+            rightIcon='arrow-top-right'
+            text='Read more'
+            href={contentUrl}
+            target='_blank'/>
+        </ButtonGroup>
+      </div>
+    </Card>
+  )
+}
+
+
+export { WikiCard }
