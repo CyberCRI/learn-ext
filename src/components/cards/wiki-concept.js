653d8bb71b7d36d2771260e399eb3dac46adf8e2
Prashant Sinha <prashant+git@noop.pw>
2019-04-03 15:38:59 +0200

    Decompose Skeleton, Error, and PageInfo cards

--- src/components/cards/wiki-concept.js
+++ src/components/cards/wiki-concept.js
@@ -1,22 +1,57 @@
 import React, { useState, useEffect } from 'react'
 import { useEffectOnce } from 'react-use'
-import { Card, Elevation, AnchorButton, ButtonGroup } from '@blueprintjs/core'
-import { FaWikipediaW } from 'react-icons/fa'
+import { Card, Elevation, AnchorButton, ButtonGroup, NonIdealState } from '@blueprintjs/core'
+import { FaWikipediaW, FaBandAid } from 'react-icons/fa'
 import clsx from 'classnames'
 import _ from 'lodash'
 
 import Wiki from '~mixins/wikipedia'
 
 
-const SkeletonCard = () => (
+export const SkeletonCard = () => (
   <Card interactive elevation={Elevation.TWO} className='info-card'>
     <h3 role='title' className='bp3-skeleton'>Boop</h3>
     <p role='text'>
-      {_.range(20).map(() =>
-        <span className='bp3-skeleton'>Boop </span>
+      {_.range(20).map((i) =>
+        <span className='bp3-skeleton' key={i}>Boop </span>
       )}
     </p>
     <div role='image' className='bp3-skeleton'/>
+  </Card>
+)
+
+export const ErrorCard = () => (
+  <Card className='info-card bp3-dark'>
+    <NonIdealState
+      icon={<FaBandAid/>}
+      description='Wikipedia seems to not have any page for this concept.'/>
+  </Card>
+)
+
+export const PageInfoCard = (props) => (
+  <Card interactive elevation={Elevation.TWO} className={clsx('info-card', 'bp3-dark')}>
+    <div className='content'>
+      <h3 className='title'>
+        {props.title}
+      </h3>
+      <p className='summary'>{props.extract}</p>
+    </div>
+    {props.thumbnail && <img className='thumbnail' src={props.thumbnail}/>}
+    <div className='tools'>
+      <ButtonGroup fill minimal>
+        <AnchorButton
+          icon='send-to-map'
+          text='Locate in Map'
+          href='#'
+          target='_blank'/>
+        <AnchorButton
+          icon={<FaWikipediaW/>}
+          rightIcon='arrow-top-right'
+          text='Read more'
+          href={props.url}
+          target='_blank'/>
+      </ButtonGroup>
+    </div>
   </Card>
 )
 
@@ -27,40 +62,18 @@ const WikiCard = (props) => {
   useEffectOnce(() => {
     Wiki.summary(props.title)
       .then(setPageInfo)
+      .fail(() => setPageInfo({ error: true }))
   })
 
   if (!pageInfo) {
     return <SkeletonCard />
   }
 
-  const contentUrl = pageInfo.content_urls.desktop.page
+  if (pageInfo.error) {
+    return <ErrorCard />
+  }
 
-  return (
-    <Card interactive elevation={Elevation.TWO} className={clsx('info-card', 'bp3-dark')}>
-      <div className='content'>
-        <h3 className='title'>
-          {pageInfo.title}
-          <cite>From Wikipedia</cite>
-        </h3>
-        <p className='summary'>{pageInfo.extract}</p>
-      </div>
-      {pageInfo.thumbnail && <img className='thumbnail' src={pageInfo.thumbnail.source}/>}
-      <div className='tools'>
-        <ButtonGroup fill minimal>
-          <AnchorButton
-            icon='send-to-map'
-            text='Locate in Map'
-            href={contentUrl}
-            target='_blank'/>
-          <AnchorButton
-            rightIcon='arrow-top-right'
-            text='Read more'
-            href={contentUrl}
-            target='_blank'/>
-        </ButtonGroup>
-      </div>
-    </Card>
-  )
+  return <PageInfoCard {...pageInfo}/>
 }
 
 
