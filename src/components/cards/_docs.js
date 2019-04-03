05e38ffa93b42e19102e6f171defca861ad2619c
Prashant Sinha <prashant+git@noop.pw>
2019-04-03 15:38:14 +0200

    Update _docs.js

--- src/components/cards/_docs.js
+++ src/components/cards/_docs.js
@@ -4,18 +4,19 @@ import { Popover, PopoverInteractionKind, Position } from '@blueprintjs/core'
 import { WikiCard } from './wiki-concept'
 
 
-const WikiPopoverCard = (props) => (
-  <Popover
-    content={<WikiCard title={props.label}/>}
-    target={<span>{props.label}</span>}
-    interactionKind={PopoverInteractionKind.HOVER}
-    hoverCloseDelay={500}
-    hoverOpenDelay={200}
-    inheritDarkTheme={false}
-    isOpen={true}
-    usePortal={false}
-    enforceFocus={false}
-    position={Position.RIGHT}/>
-)
+const WikiPopoverCard = (props) => {
+  return (
+    <Popover
+      content={<WikiCard title={props.label}/>}
+      target={<span>{props.label}</span>}
+      interactionKind={PopoverInteractionKind.HOVER}
+      hoverCloseDelay={500}
+      hoverOpenDelay={200}
+      inheritDarkTheme={false}
+      isOpen={true}
+      usePortal={false}
+      enforceFocus={false}
+      position={Position.RIGHT}/>)
+}
 
 export { WikiPopoverCard }
