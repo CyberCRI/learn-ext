7551e24e8784e89fa9d627a55b410891ed86570e
Prashant Sinha <prashant+git@noop.pw>
2019-04-03 15:36:46 +0200

    Add Runtime environment Sensor

--- src/mixins/utils.js
+++ src/mixins/utils.js
@@ -2,13 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import $ from 'cash-dom'
 import uuid_v5 from 'uuid/v5'
+import _ from 'lodash'
+import Enum from 'enum'
+
+// Enumerate the context this code is running into.
+// -> extension: Running under extension process (can access browser/chrome) API
+// -> browser: Running in a webpage
+// -> node: Running inside nodejs runtime.
+export const Runtime = new Enum([
+  'extension',
+  'browser',
+  'node',
+], { name: 'Runtime', ignoreCase: true })
 
 
-const getMetaAttribute = (name) => {
+export const getMetaAttribute = (name) => {
   return $(`meta[name="${name}"]`).attr('content')
 }
 
-const getCanonicalUrl = () => {
+export const getCanonicalUrl = () => {
   const canonical = $('link[rel="canonical"]').attr('href')
   if (canonical) {
     return canonical
@@ -17,13 +29,13 @@ const getCanonicalUrl = () => {
   }
 }
 
-const nsuuid = (param) => {
+export const nsuuid = (param) => {
   // Read as: Namespace UUID.
   // Generate a uuid within application namespace appending the parameters.
   return uuid_v5(`${env.uuid5_namespace}/${param}`, uuid_v5.URL)
 }
 
-const renderReactComponent = (selector, component, props) => {
+export const renderReactComponent = (selector, component, props) => {
   const el = document.getElementById(selector)
 
   if (el) {
@@ -39,5 +51,30 @@ const renderReactComponent = (selector, component, props) => {
   }
 }
 
+export const context = () => {
+  // Perform global variable existence test to find the context we're currently
+  // inside.
+  //
+  // Within the Extension context, either `browser` or, `chrome` are defined.
+  // Inside a webpage, `window` is defined.
+  // In nodejs runtime, `global` is defined.
+  //
+  // This function tries to see which of these three (`browser`, `chrome`,
+  // `window`) are available, and returns the Context Flag.
 
-export { renderReactComponent, getCanonicalUrl, nsuuid, getMetaAttribute }
+  try {
+    _.isObjectLike(browser)
+    return Runtime.extension
+  } catch {}
+  try {
+    _.isObjectLike(chrome)
+    return Runtime.extension
+  } catch {}
+
+  try {
+    _.isObjectLike(window)
+    return Runtime.browser
+  } catch {}
+
+  return Runtime.node
+}
