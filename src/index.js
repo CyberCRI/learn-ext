b37557b95c687e3d89d22f12592f6e534ac7305c
Prashant Sinha <prashant+git@noop.pw>
2019-04-25 16:58:29 +0200

    Cleanup content script

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import $ from 'cash-dom'
-import { renderReactComponent } from '~mixins/utils'
 
+import { renderReactComponent } from '~mixins/utils'
 import FrameContainer from '~components/iframe'
 
-
 const EXT_ROOT_ID = 'ilearn-ext-frame'
-
-const pageUrl = window.location.toString()
 const frameUrl = browser.runtime.getURL('pages/popover.html')
 
-
 const mountRootContainer = () => {
-  const frameSrc = `${frameUrl}#${pageUrl}`
+  // [!] Remove any stray element before appending the div.
+  //     By standard, no two DOM elements can have same `id`. Although it's
+  //     not strictly enforced, we'll follow the standards though.
+  $(`#${EXT_ROOT_ID}`).remove()
   $(`<div id="${EXT_ROOT_ID}"></div>`)
     .appendTo('body')
 
-  renderReactComponent(EXT_ROOT_ID, FrameContainer, { src: frameSrc })
+  renderReactComponent(EXT_ROOT_ID, FrameContainer, { src: frameUrl })
 }
 
 mountRootContainer()
