971c1b10d1be4ef27127d79bb9c18133702716f6
Prashant Sinha <prashant+git@noop.pw>
2019-05-06 00:02:23 +0200

    Refactor Pill components
    Instead of repeating props we desire, I took the Pill components out and created composed pills

--- src/components/pills/index.js
+++ src/components/pills/index.js
@@ -1,19 +1,35 @@
 import React from 'react'
 import moment from 'moment'
 import urlParse from 'url-parse'
+import clsx from 'classnames'
 
 import './styles.scss'
 
 
-export const LanguagePill = ({ lang }) => {
+export const Pill = (props) => {
+  // Pillbox Composer
+  const modifiers = clsx('pill', props.className, props.kind, {
+    minimal: props.minimal,
+    large: props.large,
+    interactive: props.interactive,
+  })
+
   return (
-    <span className='pill language'>
-      {lang}
-    </span>
+    <div role='pill' className={modifiers}>
+      { props.children }
+    </div>
   )
 }
 
-export const DateTimePill = ({ timestamp, lang }) => {
+export const LanguagePill = (props) => {
+  return (
+    <Pill kind='language' {...props}>
+      <span>{props.lang}</span>
+    </Pill>
+  )
+}
+
+export const DateTimePill = ({ timestamp, lang, ...props }) => {
   const displayTime = moment
     .utc(timestamp)
     .locale(lang)
@@ -21,18 +37,52 @@ export const DateTimePill = ({ timestamp, lang }) => {
     .calendar()
 
   return (
-    <time dateTime={timestamp} className='pill datetime'>
-      {displayTime}
-    </time>
+    <Pill kind='datetime' {...props}>
+      <time dateTime={timestamp}>{displayTime}</time>
+    </Pill>
   )
 }
 
-export const UrlPill = ({ url }) => {
+export const UrlPill = ({ url, ...props }) => {
   const uprops = urlParse(url)
+
+  // If `linked`, we render anchor element, otherwise just a span.
+  const linked = props.linked || false
+  const short = props.short || false
+
+  const hostEl = <span className='host'>{uprops.hostname}</span>
+  const pathEl = <span className='path'>{uprops.pathname}</span>
+
+  const renderUrl = () => {
+    if (linked) {
+      return (
+        <a href={url} target='_blank' rel='nofollow'>
+          { hostEl }
+          { !short && pathEl }
+        </a>
+      )
+    } else {
+      return (
+        <span className='anchor'>
+          { hostEl }
+          { !short && pathEl }
+        </span>
+      )
+    }
+  }
+
   return (
-    <a href={url} className='pill url'>
-      <span className='host'>{uprops.hostname}</span>
-      <span className='path'>{uprops.pathname}</span>
-    </a>
+    <Pill kind='url' {...props}>
+      { renderUrl() }
+    </Pill>
+  )
+}
+
+
+export const HotKeysPill = ({ keys, ...props }) => {
+  return (
+    <Pill kind='hotkey' {...props}>
+      { keys.map((i) => <kbd key={i}>{i}</kbd>) }
+    </Pill>
   )
 }
