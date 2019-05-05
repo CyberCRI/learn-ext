aedd71e205c6c8766f8ecc9941ab1c148e4ded5e
Prashant Sinha <prashant+git@noop.pw>
2019-05-06 00:04:44 +0200

    Implement resource-cards and update docs

--- src/components/cards/resources.js
+++ src/components/cards/resources.js
@@ -2,12 +2,23 @@ import React from 'react'
 import { Card, Elevation } from '@blueprintjs/core'
 
 import { ConceptList } from '~components/concepts'
+import { LanguagePill, DateTimePill, UrlPill } from '~components/pills'
 
 
 export const ResourceCard = (props) => (
-  <Card elevation={Elevation.TWO} interactive>
+  <Card elevation={Elevation.TWO} interactive className='card resource'>
     <h4 className='title'>{props.title}</h4>
-    <time dateTime={props.recorded}>{props.recorded}</time>
-    <ConceptList concepts={props.concepts} onRemove={console.log}/>
+
+    <DateTimePill timestamp={props.recorded_on} lang={props.lang}/>
+    <LanguagePill lang={props.lang}/>
+    <UrlPill url={props.url} short linked/>
+
+    <ConceptList
+      concepts={props.concepts.map((c) => ({
+        title: c[`title_${props.lang}`] || c.title_en,
+        ...c,
+      }))}
+      lang={props.lang}
+      onRemove={console.log}/>
   </Card>
 )
