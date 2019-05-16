7bccce8a322abd44d85bc38a1cba6a963396417a
Prashant Sinha <prashant+git@noop.pw>
2019-05-16 17:05:50 +0200

    Create `DashboardView` container component with OmniBar and FilterTools
    This uses a common shared state with changes propagated between all children.

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,3 +1,32 @@
+import React, { useState } from 'react'
+import { useEffectOnce, useSetState } from 'react-use'
+
+import RootAPI from '~mixins/root-api'
+import { ResourceGrid } from './resources'
+import { OmniBar, FilterTools } from './tools'
+
 import './styles.scss'
 
-export { OmniBar, FilterTools } from './tools'
+
+const DashboardView = () => {
+  const [ resources, setResources ] = useState([])
+  const [ filters, setFilters ] = useSetState({ query: '' })
+
+  useEffectOnce(() => {
+    // Fetch the portfolio data
+    RootAPI.fetchPortfolio()
+      .then((data) => {
+        setResources(data.resources)
+      })
+  })
+
+  return (
+    <div>
+      <OmniBar onChange={(q) => setFilters(q)}/>
+      <FilterTools/>
+      <ResourceGrid resources={resources} filters={filters}/>
+    </div>
+  )
+}
+
+export { ResourceGrid, OmniBar, FilterTools, DashboardView }
