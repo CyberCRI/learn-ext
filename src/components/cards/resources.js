2b2d2f651e30008d095037cf1590316de570c4ce
Prashant Sinha <prashant+git@noop.pw>
2019-05-21 15:39:02 +0200

    Render cover images from page metadata

--- src/components/cards/resources.js
+++ src/components/cards/resources.js
@@ -1,16 +1,34 @@
 import React from 'react'
+import { useMount, useAsyncFn } from 'react-use'
 import { Card, Elevation } from '@blueprintjs/core'
 
+import { request } from '~mixins/request'
 import { ConceptList } from '~components/concepts'
 import { LanguagePill, DateTimePill, UrlPill } from '~components/pills'
 
 
 export const ResourceCard = (props) => {
+  const [ ogMeta, fetchOgMeta ] = useAsyncFn(async () => {
+    return await request({
+      url: 'https://opt.ilearn.cri-paris.org/og/meta',
+      method: 'get',
+      data: {
+        url: props.url,
+      },
+    })
+  })
 
+  useMount(() => {
+    // Fetch metadata on mount.
+    fetchOgMeta()
+  })
 
   return (
     <Card elevation={Elevation.TWO} interactive className='card resource'>
       <h4 className='title'>{props.title}</h4>
+
+      { !ogMeta.loading && ogMeta.value && <img src={ogMeta.value.image}/> }
+
       <DateTimePill timestamp={props.recorded_on} lang={props.lang}/>
       <LanguagePill lang={props.lang}/>
       <UrlPill url={props.url} short linked/>
