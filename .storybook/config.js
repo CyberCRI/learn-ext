937138a247e6aeff0a2fa01d215dae66f702f0fa
Prashant Sinha <prashant+git@noop.pw>
2019-05-28 16:44:52 +0200

    Register theme and console plugins

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,16 @@
-import { configure } from '@storybook/react'
+import { configure, addParameters } from '@storybook/react'
+import displaytheme from './displaytheme'
+import '@storybook/addon-console'
 
 const loadStories = () => {
   // Import the storybook modules
   require('../docs/stories/index.js')
 }
 
-configure(loadStories, module)
+addParameters({
+  options: {
+    theme: displaytheme,
+  },
+})
 
+configure(loadStories, module)
