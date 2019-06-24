60330111d72a055460efde887cf986fa35cafca9
Prashant Sinha <prashant+git@noop.pw>
2019-06-24 11:07:09 +0200

    Add group selection in settings panel, utilising Runtime  api wrappers

--- src/components/input/settings.js
+++ src/components/input/settings.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { Callout, Intent } from '@blueprintjs/core'
-import { InputGroup, ControlGroup, Button } from '@blueprintjs/core'
+import { InputGroup, ControlGroup, Button, RadioGroup, Radio, FormGroup } from '@blueprintjs/core'
 import * as FiIcon from 'react-icons/fi'
 import _ from 'lodash'
 
-import RootAPI from '~mixins/root-api'
+import RootAPI, { RuntimeParams } from '~mixins/root-api'
 import { userId } from '~mixins/utils'
 
 
@@ -13,35 +13,45 @@ class AccountSelector extends Component {
     super(props)
     this.state = {
       username: '',
+      groupId: '',
       loading: true,
+      groups: [],
       status: 0,
     }
 
     this.didUpdateUserSettings = this.didUpdateUserSettings.bind(this)
     this.didChangeUsername = this.didChangeUsername.bind(this)
+    this.didPickGroupId = this.didPickGroupId.bind(this)
   }
 
   componentDidMount () {
-    browser.storage.local
-      .get('user')
-      .then(({ user }) => {
-        if (!_.get(user, 'signedIn')) {
-          this.setState({
-            loading: false,
-            signedIn: false,
-          })
-        } else {
-          this.setState({
-            loading: false,
-            ...user,
-          })
-        }
+    const loadParams = async () => {
+      const user = await RuntimeParams.userInfo()
+      const groups = await RuntimeParams.groupStruct()
+
+      return { user, groups }
+    }
+
+    loadParams().then((params) => {
+      // Filter the default user group
+      const { id } = _.find(params.groups, [ 'default', true ])
+
+      this.setState({
+        groupId: id,
+        loading: false,
+        groups: params.groups,
+        ...params.user,
       })
+    })
   }
 
   didChangeUsername (e) {
     // Update username input box and reset signedIn state.
     this.setState({ username: e.target.value, signedIn: false })
+  }
+
+  didPickGroupId (e) {
+    this.setState({ groupId: e.target.value, signedIn: false })
   }
 
   didUpdateUserSettings (e) {
@@ -50,13 +60,14 @@ class AccountSelector extends Component {
     this.setState({ signedIn: false, loading: true })
     if (this.state.username.length >= 2) {
       // For now, we just don't really bother about validation...
-      const username = this.state.username
+      const { username, groupId } = this.state
       RootAPI
-        .initializeUser({ email: username })
+        .initializeUser({ email: username, group_id: groupId })
         .then(() => {
           const payload = {
             uid: userId(username),
-            username: username,
+            username,
+            groupId,
             signedIn: true,
           }
           browser.storage.local
@@ -78,21 +89,32 @@ class AccountSelector extends Component {
           {this.state.signedIn && <p>You are signed in!</p>}
           {this.state.status === -1 && <p>Trouble reaching server. Please try again.</p>}
 
-          <ControlGroup >
+          <FormGroup inline label='Your Email'>
             <InputGroup
               leftIcon='id-number'
               disabled={this.state.loading}
               value={this.state.username}
               intent={inputIntent}
               onChange={this.didChangeUsername}/>
-            <Button
-              text='Save'
-              type='submit'
-              intent={inputIntent}
-              loading={this.state.loading}
-              icon={<FiIcon.FiSave/>}
-              onClick={this.didUpdateUserSettings}/>
-          </ControlGroup>
+          </FormGroup>
+
+          <RadioGroup
+            label='Group'
+            inline
+            selectedValue={this.state.groupId}
+            onChange={this.didPickGroupId}>
+            {this.state.groups.map(({ id, label }) =>
+              <Radio label={label} value={id} key={id}/>
+            )}
+          </RadioGroup>
+
+          <Button
+            text='Save'
+            type='submit'
+            intent={inputIntent}
+            loading={this.state.loading}
+            icon={<FiIcon.FiSave/>}
+            onClick={this.didUpdateUserSettings}/>
         </Callout>
       </form>
     )
