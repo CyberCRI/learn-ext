0187fe301564bba6f0d318ded0155013037dda65
Prashant Sinha <prashant+git@noop.pw>
2019-09-08 14:06:14 +0200

    Add an option to pick fonts while generating css
    This essentially allows packing only the fonts we want.

--- tools/discover-fonts.py
+++ tools/discover-fonts.py
@@ -18,7 +18,7 @@ def process_fontlist(df, urlprefix, path):
   def transform_fontname(name):
     # In CSS, it's quite annoying to use various "Medium", "SemiBold", and
     # "Light" suffixes. Here we just remove them from names.
-    pattern = r'^(.*) (Light|Medium|SemiBold|ExtraBold)$'
+    pattern = r'^(.*) (Light|Medium|SemiBold|ExtraBold|BlackItalic)$'
     try:
       return re.match(pattern, name).group(1)
     except AttributeError:
@@ -56,23 +56,27 @@ def process_fontlist(df, urlprefix, path):
   df['url_alt'] = df.file.apply(transform_url(version='woff'))
   return df
 
-def discover_fonts(path, urlprefix=''):
+def discover_fonts(path, urlprefix='', picks=[]):
   '''Use font-config to scan and list the properties in a dataframe.
 
   In the scanned directory, font-config optionally accepts an output format
   string. We use this format for each font files found:
-    >{family}|{postscriptname}|{file}
+    >{family[0]}|{postscriptname}|{file}
   I added the `>` specifier to distinguish additional output (to stderr, but
   subprocess.getoutput captures both `stdout` and `stderr` together) from
   warnings.
+
+  [NOTE]  05-09-2019
+    Modified pattern to family[0], since the font family
+    could include more than one names. We only want one.
   '''
   header = ['family', 'postscriptname', 'file']
-  fc_format = '|'.join([f'%{{{id}}}' for id in header])
+  fc_format = '%{family[0]}|%{postscriptname}|%{file}'
 
   # Uses fc-scan utility from font-config.
   cmd = f'fc-scan {path} -b -f ">{fc_format}\\n"'
 
-  return (py_(subprocess.getoutput(cmd))
+  df = (py_(subprocess.getoutput(cmd))
     .lines()
     .filter(lambda s: s[0] == '>')
     .map(lambda s: s[1:])
@@ -80,11 +84,23 @@ def discover_fonts(path, urlprefix=''):
     .thru(io.StringIO)
     .thru(lambda b: pd.read_csv(b, sep='|', names=header))
     .thru(lambda df: process_fontlist(df, urlprefix, path))
-    .value()
-  )
+    .value())
+
+  if len(picks) > 0:
+    hues.info('Picking:', picks)
+    picked_fonts = [name.lower() for name in picks]
+    frame_slice = (df
+      .family
+      .str
+      .lower()
+      .isin(picked_fonts))
+    return df[frame_slice]
+  return df
+
 
 def generate_css(df):
   '''Use the dataframe to generate an entry for each font.'''
+
   def fontface_entry(row):
     '''Generate CSS for the font entry'''
     return f'''
@@ -104,15 +120,16 @@ def generate_css(df):
 @click.option('-p', '--path', default='assets/fonts', help='Directory containing fonts')
 @click.option('-u', '--urlprefix', default='../../assets/fonts', help='Public URL Prefix')
 @click.option('-o', '--output', default='src/styles/fonts.css', help='CSS file output path')
+@click.option('-t', '--take', multiple=True)
 @click.option('--silent', is_flag=True, help='Disable logs')
-def webfonts_generator(path, urlprefix, output, silent):
+def webfonts_generator(path, urlprefix, output, take, silent):
   '''Discover and generate CSS file for the webfonts.'''
-  fonts = discover_fonts(path, urlprefix)
+  fonts = discover_fonts(path, urlprefix, take)
   with open(output, 'w') as fp:
     fp.write(generate_css(fonts))
 
   if not silent:
-    hues.success(f'[!] Found {len(fonts)} fonts in {path}.')
+    hues.success(f'[!] Found {len(fonts)} matching fonts in {path}.')
     for name, els in fonts.groupby('fname').groups.items():
       hues.info(f'    -> {name} [{len(els)} style(s)]')
 
