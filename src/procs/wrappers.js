522ca26bd2b48acd2c4e1a7d781f4f95a63616d0
Prashant Sinha <prashant+git@noop.pw>
2019-09-16 16:26:40 +0200

    Expand i18n wrapper to return last key element outside extension process

--- src/procs/wrappers.js
+++ src/procs/wrappers.js
@@ -1,20 +1,24 @@
 // Utility toolbelt for webext APIs.
 import { browser } from '~procs/stubs'
 import { context, Runtime } from '~mixins/utils'
+import _ from 'lodash'
 
 
-export const i18n = (key, ...subs) => {
+export const i18n = (key, subs) => {
   // Return localised string from locale file.
   // Note that this only works in context of the extension. Hence it should not
   // be used elsewhere outside extension process.
+  //
+  // We'll use template strings in lodash for substitutions.
   //
   // [NOTES]
   // Additionally, this function transforms the key so "dot syntax" is valid.
   // Essentially, by using yaml locale files, we are able to group messages
   // together (check Readme in assets/locales).
   if (context() === Runtime.extension) {
-    return browser.i18n.getMessage(key.replace(/\./g, '_'), subs)
+    const msg = browser.i18n.getMessage(key.replace(/\./g, '_'))
+    return _.template(msg)(subs)
   } else {
-    return key
+    return key.split('.').slice(-1)
   }
 }
