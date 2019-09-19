816e027d32114fc24a8ab9fb287e0bcc414534ce
Prashant Sinha <prashant+git@noop.pw>
2019-09-19 16:05:08 +0200

    Implement stub for localstorage in ext and web contexts

--- src/mixins/persistence.js
+++ src/mixins/persistence.js
@@ -1,10 +1,37 @@
+import { browser } from '~procs/stubs'
 import { context, Runtime } from '~mixins/utils'
 
 
-class Store {
-  constructor () {
-    if (context() === Runtime.extension) {
-      // Use extension localstorage.
-    }
+class ExtensionStorage {
+  async get (key) {
+    const value = await browser.storage.local.get(key)
+    return JSON.parse(value[key])
+  }
+
+  async set (key, value) {
+    await browser.storage.local.set({
+      [key]: JSON.stringify(value),
+    })
   }
 }
+
+class WebStorage {
+  async get (key) {
+    const value = window.localStorage.getItem(key)
+    return JSON.parse(value)
+  }
+
+  async set (key, value) {
+    window.localStorage.setItem(key, JSON.stringify(value))
+  }
+}
+
+let store
+
+if (context() === Runtime.extension) {
+  store = new ExtensionStorage()
+} else {
+  store = new WebStorage()
+}
+
+export default store
