33b138b5f7ba9b485d9ce79297a3ec273d32d55d
Prashant Sinha <prashant+git@noop.pw>
2019-10-07 11:04:49 +0200

    Use web-ext i18n api directly for context menus

--- src/procs/contextMenus.js
+++ src/procs/contextMenus.js
@@ -1,18 +1,25 @@
 import { browser } from '~procs/stubs'
 import { ExtensionPages } from './reactors'
-import { i18n } from './wrappers'
+import _ from 'lodash'
+
+
+const i18nT = (key) => {
+  // Tiny helper for resolving locale phrases from webext.i18n API.
+  return browser.i18n.getMessage(key.replace(/\./g, '_'))
+}
+
 
 const attachActionMenu = () => {
   // Attach a menu button for shortcuts to the dashboard page.
   browser.contextMenus.create({
     contexts: ['browser_action'],
-    title: i18n('menu.action.openDashboard'),
+    title: i18nT('menu.action.openDashboard'),
     onclick: ExtensionPages.dashboard.open,
   })
 
   browser.contextMenus.create({
     contexts: ['browser_action'],
-    title: i18n('menu.action.openMap'),
+    title: i18nT('menu.action.openMap'),
     onclick: ExtensionPages.discover.open,
   })
 }
@@ -20,7 +27,7 @@ const attachActionMenu = () => {
 const attachPageMenu = (handler) => {
   browser.contextMenus.create({
     contexts: ['page'],
-    title: i18n('menu.action.addResource'),
+    title: i18nT('menu.action.addResource'),
     documentUrlPatterns: ['http://*/*', 'https://*/*'],
     onclick: handler,
   })
