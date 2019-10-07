9cb7d96472e2966ae8ce2532aec5b8889e915b10
Prashant Sinha <prashant+git@noop.pw>
2019-10-07 11:08:34 +0200

    Switch navbar labels to modules/i18n

--- src/pages/_commons/navigation-bar.js
+++ src/pages/_commons/navigation-bar.js
@@ -2,31 +2,32 @@ import React from 'react'
 import { Navbar, Alignment, AnchorButton } from '@blueprintjs/core'
 import * as FiIcon from 'react-icons/fi'
 
-import { i18n } from '~procs/wrappers'
+import { i18n } from '@ilearn/modules/i18n'
 
+const i18nT = i18n.context('navigationBar')
 
 const NavigationBar = () => (
   // Render Navigation Bar
   <Navbar className='np-navbar bp3-dark'>
     <Navbar.Group align={Alignment.LEFT}>
-      <Navbar.Heading>{i18n('navigationBar.heading')}</Navbar.Heading>
+      <Navbar.Heading>{i18nT('heading')}</Navbar.Heading>
       <Navbar.Divider/>
 
       <AnchorButton
-        text={i18n('navigationBar.links.dashboard.label')}
+        text={i18nT('links.dashboard.label')}
         minimal
         href='/pages/dashboard.html'
         icon='book'/>
 
       <AnchorButton
-        text={i18n('navigationBar.links.discover.label')}
+        text={i18nT('links.discover.label')}
         minimal
         href='/pages/options.html'
         icon={<FiIcon.FiCompass/>}/>
     </Navbar.Group>
     <Navbar.Group align={Alignment.RIGHT}>
       <AnchorButton
-        text={i18n('navigationBar.links.settings.label')}
+        text={i18nT('links.settings.label')}
         minimal
         href='/pages/settings.html'
         icon={<FiIcon.FiSettings/>}/>
