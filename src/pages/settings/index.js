3673d26b68460f511c0d2d9eba40ee02273255e1
Prashant Sinha <prashant+git@noop.pw>
2019-10-07 11:08:18 +0200

    Add localised page title for settings

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { renderReactComponent } from '~mixins/react-helpers'
-import * as FiIcon from 'react-icons/fi'
-import { IconContext } from 'react-icons'
+import { Helmet } from 'react-helmet'
+
 import SettingsTabs from './tabs'
+import { i18n } from '@ilearn/modules/i18n'
 import './style.sass'
 
 
 const SettingsContainer = (props) => {
   return (
     <div>
-
+      <Helmet>
+        <title>{i18n.t('pages.settings.meta.pageTitle')}</title>
+      </Helmet>
       <SettingsTabs />
     </div>
   )
