4cde7077f440a12650739751f929828c84d5b54b
Prashant Sinha <prashant+git@noop.pw>
2019-11-05 11:57:28 +0100

    Implement first version of discover tab overlays

--- src/views/discover/overlays.js
+++ src/views/discover/overlays.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import { useStore } from 'effector-react'
+import _ from 'lodash'
 
-import { ResourceCard } from '~components/cards/resources'
+import { ResourceCollectionView } from '~components/dashboard'
+import { ConceptList } from '~components/concepts'
+import { selectedConcepts, userResourcesIndex } from './store'
 
-export const OverlayCards = () => {
+export const OverlayCards = (props) => {
+  const conceptList = useStore(selectedConcepts)
+  const resourceIndex = userResourcesIndex.getState()
+
+  const matchingResources = _(conceptList)
+    .filter('userData')
+    .map((c) => resourceIndex.search(c.title))
+    .flatten()
+    .uniqBy('resource_id')
+    .value()
+
+  if (conceptList.length > 0) {
+    return (
+      <div className='matches'>
+        <ConceptList concepts={conceptList}/>
+        <ResourceCollectionView resources={matchingResources} skipConceptList/>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <ResourceCard />
+    <div className='matches'>
+      <p>Pick concepts from the map above to see matching resources.</p>
     </div>
   )
 }
