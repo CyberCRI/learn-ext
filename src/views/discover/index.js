e6a7a212e245d15d370e273b1163e7b55eb34491
Prashant Sinha <prashant+git@noop.pw>
2019-11-05 11:57:52 +0100

    Refactor DiscoverView setup method

--- src/views/discover/index.js
+++ src/views/discover/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
-import { setupInstance } from './renderer'
+import { i18n } from '@ilearn/modules/i18n'
+import { setupMapView } from './renderer'
+import { fetchResources } from './store'
 import { OverlayCards } from './overlays'
 
 const DiscoverView = () => {
   return (
     <div className='discover'>
-      <h1>Browse Resources on Map</h1>
-      <OverlayCards />
+      <Helmet>
+        <title>{i18n.t('pages.discover.meta.pageTitle')}</title>
+      </Helmet>
+      <OverlayCards/>
     </div>
   )
+}
+
+const setupInstance = async (config) => {
+  setupMapView(config)
+  fetchResources()
 }
 
 export { DiscoverView, setupInstance }
