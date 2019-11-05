d20e6f4262e2265c1807bff5a6ea2b0ee9afcb6b
Prashant Sinha <prashant+git@noop.pw>
2019-11-05 11:46:47 +0100

    Add redirect handler for both dashboard and discover tabs

--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,17 +1,20 @@
 import { renderReactComponent } from '~mixins/react-helpers'
 import { DiscoverView, setupInstance } from '~views/discover'
 
+import { ensureLogin } from '~components/input/loginSensor'
+
 import './style.scss'
 
-
-document.addEventListener('apploaded', () => {
+const init = () => {
   window.atlas = setupInstance({
     element: document.getElementById('atlas'),
     pixelRatio: window.devicePixelRatio || 1,
-    onClick: (e) => {
-      console.log(e)
-    },
   })
 
   renderReactComponent('discover', DiscoverView)
+}
+
+document.addEventListener('apploaded', () => {
+  ensureLogin({ autoRedirect: true })
+    .then(init)
 })
