35cdeddaeaa8aae99d2dd6804d3be58f1120b22c
Prashant Sinha <prashant+git@noop.pw>
2019-11-06 15:27:53 +0100

    Refactor store to utilise derived states via query index

--- src/views/discover/store.js
+++ src/views/discover/store.js
@@ -1,7 +1,8 @@
 import { createEvent, createEffect, createStore } from 'effector'
 import { Set } from 'immutable'
 import { API } from '@ilearn/modules/api'
-import Fuse from 'fuse.js'
+import _ from 'lodash'
+import { conceptIndexSet, resourceIndex, matchQuerySet } from './query-index'
 
 export const conceptSelection = {
   merge: createEvent(),
@@ -10,32 +11,39 @@ export const conceptSelection = {
   remove: createEvent(),
 }
 
-export const fetchResources = createEffect({
-  handler: async () => {
-    const resources = await API.resources({ limit: 100 })
-    return resources.results
-  },
-})
+export const fetchResources = createEffect()
+  .use(async ({ limit=100, start=1 }) => {
+    const response = await API.resources({ limit, start })
+    const offset = response.pagination.start + response.pagination.limit
+    if (offset < response.pagination.count) {
+      console.log(`[Resources] Fetching next batch <limit: ${limit}, start: ${offset}>`)
+      fetchResources({ limit, start: offset })
+    }
+    return response.results
+  })
 
 export const selectedConcepts = createStore(Set())
   .on(conceptSelection.merge, (state, vals) => state.union(Set(vals)))
   .on(conceptSelection.replace, (_, vals) => Set(vals))
   .on(conceptSelection.reset, () => Set())
   .on(conceptSelection.remove, (state, vals) => state.subtract(Set(vals)))
-  .map((state) => state.toJS())
+
+export const conceptsQuerySet = selectedConcepts
+  .map((state, args) => conceptIndexSet(state.toJS()))
 
 
 export const userResources = createStore([])
-  .on(fetchResources.done, (_, r) => r)
+  .on(fetchResources.done, (state, r) => [...state, ...r.result])
 
 export const userResourcesIndex = userResources
-  .map((state) => {
-    return new Fuse(state.result, {
-      shouldSort: true,
-      threshold: 0, // Perfect match
-      keys: [
-        'concepts.title_fr',
-      ],
-    })
-  })
+  .map((state) => resourceIndex(state))
 
+export const matchingConceptSet = conceptsQuerySet
+  .map((state) => matchQuerySet(userResourcesIndex.getState(), state))
+
+export const matchingResourceSet = matchingConceptSet
+  .map((state) => {
+    return _(userResources.getState())
+      .filter((r) => state.has(r.resource_id))
+      .value()
+  })
