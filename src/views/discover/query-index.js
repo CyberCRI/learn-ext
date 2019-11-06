5b478372f4732f020d0821b9dc0385e200d7627f
Prashant Sinha <prashant+git@noop.pw>
2019-11-06 15:27:20 +0100

    Implement queryindex for matching concepts to resources

---
+++ src/views/discover/query-index.js
@@ -0,0 +1,41 @@
+// Implements a query-index search algorithm to find subset matches in a graph
+// like dataset.
+//
+// We're given a set {R} of resources, R, containing a unique id and a set of
+// concepts, {C}. Each concept in set {C} contains another unique id.
+//
+// Consider a query set of concepts, {C}q.
+// Let's define an ordered pair <w, R>, where w is a weight, w >= 0, and
+// R ∈ {R}. We'll use `wR` to represent this pair.
+//
+// The goal here is to build a subset of {R} using:
+//  { R.concepts ∈ {C}q ∀ R ∈ {R} }
+//
+// The weight w is currently defined as:
+//  w(R, {C}q) = n(R.concepts ∩ {C}q ∀ R ∈ {R}).
+//
+// The goal here is to transform {R} with {C}q as:
+//  { <wi = w(R, {C}q), Ri> : wi > 0 ∀ Ri ∈ {R} }
+import _ from 'lodash'
+import { Set, Map } from 'immutable'
+
+export const conceptIndexSet = (concepts=[]) => {
+  // Transform `concepts` to a set of { concept.wikiDataId }.
+  return _(concepts)
+    .map((c) => c.wikiDataId || c.wikidata_id)
+    .thru(Set)
+    .value()
+}
+
+export const resourceIndex = (resources) => {
+  // Transform `resources` to a hash table keyed by `resource_id`, mapping to
+  // `resource.concepts` set.
+  return _(resources)
+    .map((r) => ([ r.resource_id, conceptIndexSet(r.concepts) ]))
+    .thru(Map)
+    .value()
+}
+
+export const matchQuerySet = (resIndex, queryIndex) => {
+  return resIndex.filter((cset) => cset.intersect(queryIndex).size > 0)
+}
---
+++ tests/contexts/search.query-index.test.js
@@ -0,0 +1,88 @@
+import { Set, Map } from 'immutable'
+import { jsonFixture } from '~test-mixins'
+import { conceptIndexSet, resourceIndex, matchQuerySet } from '~views/discover/query-index'
+
+describe('~search/query-index', () => {
+  let sample = {}
+  before(async () => {
+    sample.concepts = await jsonFixture('api.user-concept.20191105')
+    sample.resources = await jsonFixture('api.user-resource.20191105')
+  })
+
+  context('#conceptIndexSet', () => {
+    it('builds index from concept set', () => {
+      const csetidx = conceptIndexSet(sample.concepts.results)
+      expect(csetidx)
+        .to.satisfy(Set.isSet)
+      expect(csetidx.size)
+        .to.exist
+      expect(csetidx.has(sample.concepts.results[0].wikidata_id))
+        .to.be.true
+    })
+  })
+
+  context('#resourceIndex', () => {
+    it('builds resource index mapping set', () => {
+      const resIdx = resourceIndex(sample.resources.results)
+      expect(resIdx)
+        .to.satisfy(Map.isMap)
+    })
+
+  })
+
+  context('#matchQuerySet', () => {
+    it('matches resources with one query concept', () => {
+      const queryConceptId = 'Q1628157'
+      const expectedResIds = [
+        '9618724f68bf4f9a546011cf71866ce9',
+        '2ccb1b610359fb610dd499dbf734235d',
+      ]
+      const resIdx = resourceIndex(sample.resources.results)
+      const query = Set([ queryConceptId ])
+
+      const matches = matchQuerySet(resIdx, query)
+      expect(matches.size)
+        .to.equal(2)
+      expect(matches)
+        .to.satisfy((m) => {
+          return m.has(expectedResIds[0]) && m.has(expectedResIds[1])
+        })
+    })
+
+    it('matches resources with multi concepts', () => {
+      const resIdx = resourceIndex(sample.resources.results)
+      const results = Set([
+        '90e3ac5174caf43d30f150e0eef243a9',
+        'd34133dfa68376b5fe88f8de72c9a3ae',
+        '97b927b58550c5e8f63f2e3680c57697',
+        '8207d4c5d00c665644d41dff92f928b6',
+      ])
+
+      const query1 = Set([
+        'Q17637401',
+        'Q544',
+        'Q460791',  // "Thomas Wright", only links to one resource.
+      ])
+      const query2 = Set([
+        'Q17637401',
+        'Q544',
+      ])
+      const query3 = Set([ 'Q460791' ]) // Should only match last one.
+
+      const match1 = matchQuerySet(resIdx, query1)
+      const match2 = matchQuerySet(resIdx, query2)
+      const match3 = matchQuerySet(resIdx, query3)
+
+      expect(match1.size)
+        .to.equal(4)
+      expect(match2.size)
+        .to.equal(4)
+      expect(match3.size)
+        .to.equal(1)
+
+      expect(match1.keySeq().toSet().equals(results)).to.be.true
+      expect(match1.equals(match2)).to.be.true
+      expect(match1.isSuperset(match3)).to.be.true
+    })
+  })
+})
