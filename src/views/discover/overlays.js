997d97b5e26fa991ac55a023878abbcbb83d365f
Prashant Sinha <prashant+git@noop.pw>
2019-11-06 15:29:02 +0100

    Refactor overlay views with updated store

--- src/views/discover/overlays.js
+++ src/views/discover/overlays.js
@@ -1,34 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useStore } from 'effector-react'
+import { Button, RadioGroup, Radio } from '@blueprintjs/core'
+import { motion } from 'framer-motion'
 import _ from 'lodash'
 
 import { ResourceCollectionView } from '~components/dashboard'
 import { ConceptList } from '~components/concepts'
-import { selectedConcepts, userResourcesIndex } from './store'
+import { selectedConcepts, matchingResourceSet, matchingConceptSet } from './store'
+
+
+const overlayControlVariants = {
+  open: {
+    x: 0,
+    scaleX: 1,
+    opacity: 1,
+  },
+  closed: {
+    x: '-100%',
+    scaleX: 0,
+    opacity: 0,
+  },
+}
+
+export const OverlayConcepts = (props) => {
+  const conceptList = useStore(selectedConcepts)
+  const [ isOpen, setPanelVisibility ] = useState(true)
+
+  return (
+    <motion.div initial='closed' animate={isOpen ? 'open' : 'closed'} variants={overlayControlVariants}>
+      <h3>Concept Map</h3>
+      <Button icon='menu-closed' onClick={() => setPanelVisibility(!isOpen)}/>
+      <RadioGroup label='Show from resources:'>
+        <Radio label='My Resources' value='user'/>
+        <Radio label='My Group' value='group'/>
+        <Radio label='All Resources' value='all'/>
+      </RadioGroup>
+
+      {!conceptList.size && <p>Pick a region on the map to show concepts</p>}
+      {!!conceptList.size && <p>Showing resources matching {conceptList.size} concepts</p>}
+      <ConceptList concepts={conceptList.toJS()}/>
+    </motion.div>
+  )
+}
 
 export const OverlayCards = (props) => {
-  const conceptList = useStore(selectedConcepts)
-  const resourceIndex = userResourcesIndex.getState()
+  const matchingResources = useStore(matchingResourceSet)
 
-  const matchingResources = _(conceptList)
-    .filter('userData')
-    .map((c) => resourceIndex.search(c.title))
-    .flatten()
-    .uniqBy('resource_id')
-    .value()
-
-  if (conceptList.length > 0) {
+  if (!matchingResources.length) {
     return (
-      <div className='matches'>
-        <ConceptList concepts={conceptList}/>
-        <ResourceCollectionView resources={matchingResources} skipConceptList/>
+      <div className='empty'>
+        <h2>Browse resources on map</h2>
+        <p>Pick a region (or several) by clicking on the map. You can refine your
+        selection by zooming in, and select several regions by holding <kbd>shift</kbd>
+        while clicking.</p>
       </div>
     )
   }
 
   return (
     <div className='matches'>
-      <p>Pick concepts from the map above to see matching resources.</p>
+      <ResourceCollectionView resources={matchingResources} skipConceptList/>
     </div>
   )
 }
