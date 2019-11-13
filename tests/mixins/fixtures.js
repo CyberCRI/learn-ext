64cd36b7c5c4c3056e7063405197b5124542f183
Prashant Sinha <prashant+git@noop.pw>
2019-11-13 12:02:50 +0100

    Add tests for discover.layers helper

--- tests/mixins/fixtures.js
+++ tests/mixins/fixtures.js
@@ -1,10 +1,14 @@
-import { request } from '~mixins'
+import reqwest from 'reqwest'
+
 
 export const jsonFixture = async (key) => {
   // Return json file fixture from global __FIXTURES__ object.
   // Transforms the file content as js object.
   const keyname = /\.json$/.test(key) ? key : `${key}.json`
-  return request({
+  return reqwest({
     url: `/base/tests/fixtures/${keyname}`,
+    type: 'json',
+    method: 'get',
+    contentType: 'application/json',
   })
 }
