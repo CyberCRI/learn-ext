bb9d678310408b1d49afb37960a4efb462c22daa
Prashant Sinha <prashant+git@noop.pw>
2019-11-13 12:04:47 +0100

    Update overlays to add help, toolbars, concept lists

--- src/views/discover/overlays.js
+++ src/views/discover/overlays.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import { createEvent, createStore, createApi } from 'effector'
 import { useStore } from 'effector-react'
 import { Button, ButtonGroup, Divider } from '@blueprintjs/core'
+import { Popover, Menu, Dialog } from '@blueprintjs/core'
 import { motion } from 'framer-motion'
 import _ from 'lodash'
 
@@ -8,36 +10,91 @@ import { i18n } from '@ilearn/modules/i18n'
 import { ResourceCollectionView } from '~components/dashboard'
 import { ConceptList } from '~components/concepts'
 import { selectedConcepts, matchingResourceSet, matchingConceptSet } from './store'
-
+import { pickLayer, resourcesDomain } from './store'
 
 const overlayControlVariants = {
   open: {
-    x: 0,
-    scaleX: 1,
+    height: 'auto',
     opacity: 1,
   },
   closed: {
-    x: '-100%',
-    scaleX: 0,
+    height: 0,
     opacity: 0,
   },
 }
 
+const hotkeysDialogState = createStore(false)
+const hotkeysDialog = createApi(hotkeysDialogState, {
+  open: () => true,
+  close: () => false,
+})
+
+export const MapKeyboardShortcutsDialog = (props) => {
+  const isOpen = useStore(hotkeysDialogState)
+  return (
+    <Dialog
+      isOpen={isOpen}
+      title='Keyboard Shortcuts'
+      icon='key-command'
+      onClose={() => hotkeysDialog.close()}>
+      <p><kbd>c</kbd> Clear Selection</p>
+    </Dialog>
+  )
+}
+
+export const MapDropdownMenuContent = (props) => {
+  return (
+    <Menu>
+      <Menu.Item
+        icon='key-command'
+        text='Keyboard Shortcuts'
+        onClick={() => hotkeysDialog.open()}/>
+      <Menu.Item
+        icon='lightbulb'
+        text='Using Discover Map'/>
+    </Menu>
+  )
+}
+
+export const MapDropdownMenu = () => {
+  return (
+    <Popover position='bottom'>
+      <Button minimal icon='more'/>
+      <div>
+        <MapDropdownMenuContent/>
+      </div>
+    </Popover>
+  )
+}
+
 export const LayerSelection = (props) => {
   const i18nT = i18n.context('pages.discover.sections.atlas.layers')
+  const layerDomain = useStore(resourcesDomain)
+  const layerChoices = [
+    { key: 'user', icon: 'layout-circle' },
+    { key: 'group', icon: 'layout-group-by' },
+    { key: 'everything', icon: 'layout-sorted-clusters' },
+  ]
+
   return (
     <div className='overlay tools'>
       <div>
       </div>
       <div>
         <ButtonGroup alignText='center' minimal className='layers'>
-          <Button icon='layout-circle' active text={i18nT('user')}/>
-          <Button icon='layout-group-by' text={i18nT('group')}/>
-          <Button icon='layout-sorted-clusters' text={i18nT('everything')}/>
+          {layerChoices.map(({ key, icon }) => (
+            <Button
+              key={key}
+              icon={icon}
+              text={i18nT(key)}
+              active={key === layerDomain}
+              onClick={() => pickLayer(key)}/>
+          ))}
         </ButtonGroup>
       </div>
       <div>
-        <Button icon='more' minimal/>
+        <MapDropdownMenu/>
+        <MapKeyboardShortcutsDialog/>
       </div>
     </div>
   )
@@ -53,18 +110,35 @@ export const OverlayTools = (props) => {
 
 export const OverlayConcepts = (props) => {
   const conceptList = useStore(selectedConcepts)
-  const [ isOpen, setPanelVisibility ] = useState(true)
+  const [ isOpen, setPanelVisibility ] = useState(false)
+
+  const label = conceptList.size ? `Concepts (${conceptList.size})` : 'Concepts'
 
   return (
     <motion.div
       className='overlay concepts'
       initial='closed'
-      animate={isOpen ? 'open' : 'closed'}
-      variants={overlayControlVariants}>
+      animate={isOpen ? 'open' : 'closed'}>
 
-      {!conceptList.size && <p>Pick a region on the map to show concepts</p>}
-      {!!conceptList.size && <p>Showing resources matching {conceptList.size} concepts</p>}
-      <ConceptList concepts={conceptList.toJS()}/>
+      <div className='controls'>
+        <Button
+          text={label}
+          rightIcon={isOpen ? 'chevron-down' : 'chevron-up'}
+          className='collapse'
+          small
+          fill
+          minimal
+          onClick={() => setPanelVisibility(!isOpen)}/>
+      </div>
+
+      <motion.div className='region' variants={overlayControlVariants}>
+        <div>
+          {!conceptList.size && <p>Pick a region on the map to show concepts</p>}
+          {!!conceptList.size && <p>Showing resources matching {conceptList.size} concepts</p>}
+
+          <ConceptList concepts={conceptList.toJS()}/>
+        </div>
+      </motion.div>
     </motion.div>
   )
 }
