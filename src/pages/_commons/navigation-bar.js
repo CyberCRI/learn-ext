a4fe41459192fcf5a97937bd321355a5abd26799
Prashant Sinha <prashant+git@noop.pw>
2019-11-19 11:25:39 +0100

    Refactor navbar to use blueprintjs icons

--- src/pages/_commons/navigation-bar.js
+++ src/pages/_commons/navigation-bar.js
@@ -1,38 +1,38 @@
 import React from 'react'
 import { Navbar, Alignment, AnchorButton } from '@blueprintjs/core'
-import * as FiIcon from 'react-icons/fi'
 
 import { i18n } from '@ilearn/modules/i18n'
 
-const i18nT = i18n.context('navigationBar')
 
-const NavigationBar = () => (
-  // Render Navigation Bar
-  <Navbar className='np-navbar bp3-dark'>
-    <Navbar.Group align={Alignment.LEFT}>
-      <Navbar.Heading>{i18nT('heading')}</Navbar.Heading>
-      <Navbar.Divider/>
+const NavigationBar = () => {
+  const i18nT = i18n.context('navigationBar')
+  return (
+    <Navbar className='np-navbar bp3-dark'>
+      <Navbar.Group align={Alignment.LEFT}>
+        <Navbar.Heading>{i18nT('heading')}</Navbar.Heading>
+        <Navbar.Divider/>
 
-      <AnchorButton
-        text={i18nT('links.dashboard.label')}
-        minimal
-        href='/pages/dashboard.html'
-        icon='book'/>
+        <AnchorButton
+          text={i18nT('links.dashboard.label')}
+          minimal
+          href='/pages/dashboard.html'
+          icon='book'/>
 
-      <AnchorButton
-        text={i18nT('links.discover.label')}
-        minimal
-        href='/pages/discover.html'
-        icon={<FiIcon.FiCompass/>}/>
-    </Navbar.Group>
-    <Navbar.Group align={Alignment.RIGHT}>
-      <AnchorButton
-        text={i18nT('links.settings.label')}
-        minimal
-        href='/pages/settings.html'
-        icon={<FiIcon.FiSettings/>}/>
-    </Navbar.Group>
-  </Navbar>
-)
+        <AnchorButton
+          text={i18nT('links.discover.label')}
+          minimal
+          href='/pages/discover.html'
+          icon='mountain'/>
+      </Navbar.Group>
+      <Navbar.Group align={Alignment.RIGHT}>
+        <AnchorButton
+          text={i18nT('links.settings.label')}
+          minimal
+          href='/pages/settings.html'
+          icon='settings'/>
+      </Navbar.Group>
+    </Navbar>
+  )
+}
 
 export default NavigationBar
