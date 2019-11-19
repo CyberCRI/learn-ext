73df363b8c4a8d79724dcdb41d77e1dbd4082786
Prashant Sinha <prashant+git@noop.pw>
2019-11-19 11:30:39 +0100

    Define IconStack structures

--- src/procs/structs.js
+++ src/procs/structs.js
@@ -5,3 +5,10 @@ export const InstallEventReason = {
   updated: 'update',
   browserUpdated: 'browser_update',
 }
+
+export const IconStack = {
+  action: {
+    idle: 'icons/extension/anchor-idle.png',
+    active: 'icons/extension/anchor-active.png',
+  },
+}
