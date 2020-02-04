8d1049d90f5852b686144b685a19c3514c3509b8
Prashant Sinha <prashant+git@noop.pw>
2020-02-04 16:56:58 +0100

    Refactor pages root

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,14 @@
 import '~styles/fonts.css'
 import './style.sass'
 
-import $ from 'cash-dom'
 import { NavigationBar, initNotifications } from '~page-commons'
 import { renderReactComponent } from '~mixins/react-helpers'
-import store from '~mixins/persistence'
 
 
 // Common page initializers
 const AppDidLoad = new Event('apploaded')
 
 window.addEventListener('load', async () => {
-  const appLang = await store.get('pref.lang') || 'en'
-
-  // Add language attribute to document
-  $('html').attr('lang', appLang)
-  // Add a css class to body
-  $('body').addClass(`lang-${appLang}`)
-
-  renderReactComponent('navbar', NavigationBar)
-  initNotifications()
-
   document.dispatchEvent(AppDidLoad)
+  renderReactComponent('navbar', NavigationBar)
 })
