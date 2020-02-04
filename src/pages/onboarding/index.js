b70c47dc6185f4fb9ea6b74fc9a5e1964f2616f1
Prashant Sinha <prashant+git@noop.pw>
2020-02-04 16:49:14 +0100

    Refactor onboarding index page

--- src/pages/onboarding/index.js
+++ src/pages/onboarding/index.js
@@ -1,20 +1,13 @@
-import React from 'react'
 import ScrollTrigger from '@terwanerik/scrolltrigger'
 
 import { renderReactComponent } from '~mixins/react-helpers'
 
-import { LoginSignupButton, DemoCards } from './presentation'
+import { DemoCards } from './presentation'
 import { DownloadLinks } from './download-buttons'
 
 import './style.scss'
 import 'animate.css/animate.css'
 
-
-const OnboardView = () => (
-  <>
-    <LoginSignupButton/>
-  </>
-)
 
 const initTriggers = async () => {
   const trigger = new ScrollTrigger({
@@ -32,11 +25,10 @@ const initTriggers = async () => {
 
 const initComponents = async () => {
   renderReactComponent('download-buttons', DownloadLinks)
-  renderReactComponent('login-button', OnboardView)
   renderReactComponent('demo-cards', DemoCards)
 }
 
 document.addEventListener('apploaded', () => {
-  initTriggers()
   initComponents()
+  initTriggers()
 })
