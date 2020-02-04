fe7e7ed1f16cf465010bdc7efb6cc822c8a4d7f7
Prashant Sinha <prashant+git@noop.pw>
2020-02-04 16:51:52 +0100

    Refactor dashboard error states

--- src/components/dashboard/placeholders.js
+++ src/components/dashboard/placeholders.js
@@ -1,20 +1,32 @@
 import React from 'react'
+import { i18n } from '@ilearn/modules/i18n'
 
 export const NoMatches = () => {
+  const i18nT = i18n.context('pages.dashboard.resourceLists.states.noMatch')
   return (
-    <div className='grid error'>
-      <h3>No Matches</h3>
-      <p>Can't find any resource matching.</p>
-      <p>You can see approximate matches or try again.</p>
+    <div className='grid error no-match'>
+      <h3>{i18nT('title')}</h3>
+      <p>{i18nT('description')}</p>
     </div>
   )
 }
 
 export const NoContent = () => {
+  const i18nT = i18n.context('pages.dashboard.resourceLists.states.empty')
+  return <>
+    <div className='grid error empty'>
+      <h3>{i18nT('title')}</h3>
+      <p>{i18nT('description')}</p>
+    </div>
+  </>
+}
+
+export const ErrorDescription = (props) => {
+  const i18nT = i18n.context('pages.dashboard.resourceLists.states.error')
   return (
-    <div className='grid error'>
-      <h3>No Content</h3>
-      <p>This is a placeholder message explaining why this dashboard is empty</p>
+    <div className='resources error server'>
+      <h3>{i18nT('title')}</h3>
+      <p>{i18nT('description')}</p>
     </div>
   )
 }
