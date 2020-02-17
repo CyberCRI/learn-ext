4e9ad2a32d81ca20b3744b9350565fe12d903de8
Prashant Sinha <prashant+git@noop.pw>
2020-02-17 13:31:49 +0100

    Refactor localstorage mixin

--- modules/mixins/index.js
+++ modules/mixins/index.js
@@ -1,4 +1,13 @@
-import { isUndefined } from 'lodash'
+// Wraps LocalStorage access which encodes objects to JSON.
+// There are a couple of other similar mixins in this code base. I haven't yet
+// identified where can they be generalised. Until then, we'll keep copies to
+// avoid leaky abstraction here.
+//
+// [!todo] Identify if a fallback is needed (and possible) for when LocalStorage
+// is not available. It may be possible to use client-side cookies but that
+// doesn't sound pretty.
+//
+// [!todo] Is it necessary to encode values in JSON?
 
 const attemptParseJson = (value) => {
   try {
@@ -8,10 +17,25 @@ const attemptParseJson = (value) => {
   }
 }
 
+const isStorageAvailable = () => {
+  try {
+    // Attempt to access localstorage. If it works, we're fine. Otherwise no
+    // access to localstorage available.
+    window.localStorage.length
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
 export const LocalStorage = {
   get (key, fallback) {
+    if (!isStorageAvailable()) {
+      console.warn('LocalStorage unavailable.')
+      return fallback
+    }
     const value = window.localStorage.getItem(key)
-    if (isUndefined(value)) {
+    if (typeof value === undefined) {
       return fallback
     }
     return attemptParseJson(value)
