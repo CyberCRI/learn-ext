2f91f5587f6afc6b48c144f573dde2d0e7c6f695
Prashant Sinha <prashant+git@noop.pw>
2020-02-17 13:26:22 +0100

    Set blueprintjs context for FocusStyle Outlines

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import '~styles/fonts.css'
 import './style.sass'
+import { FocusStyleManager } from '@blueprintjs/core'
 
 import { NavigationBar } from '~page-commons'
 import { renderReactComponent } from '~mixins/react-helpers'
-
 
 // Common page initializers
 const AppDidLoad = new Event('apploaded')
 
 window.addEventListener('load', async () => {
-  document.dispatchEvent(AppDidLoad)
+  FocusStyleManager.onlyShowFocusOnTabs()
   renderReactComponent('navbar', NavigationBar)
+
+  document.dispatchEvent(AppDidLoad)
 })
