21769eabbe35f65f20fd1dd9f5277197b2ebb4ce
Prashant Sinha <prashant+git@noop.pw>
2020-03-05 17:06:22 +0100

    Refactor dat.GUI init function to be a bit smarter

--- src/views/discover/renderer-debugger.js
+++ src/views/discover/renderer-debugger.js
@@ -1,4 +1,5 @@
 import * as datGui from 'dat.gui'
+import { LayerPropConstraints, LayerProps } from './consts'
 
 const stateSet = {
   stats: {
@@ -13,37 +14,7 @@ const stateSet = {
     y: 1,
     scale: 1,
   },
-  elevation: {
-    visible: true,
-    contourWidth: .5,
-    contourOpacity: .5,
-    elevationPow: 1,
-    lightAltitude: 10,
-    lightAzimuth: 0,
-    lightIntensity: .2,
-    maxRadiusDivider: 15,
-    elevationOffset: 0,
-    lightness: 0,
-    saturation: 0,
-  },
-  marker: {
-    visible: true,
-    markerSizeMultiplier: 5,
-    markerStrokeWidth: 0,
-    markerOpacity: 1,
-    minAbsoluteMarkerSize: 0,
-    pointHoverRadiusMultiplier: 10,
-    markerFillOpacity: 1,
-    markerStrokeOpacity: 0,
-  },
-  labels: {
-    visible: true,
-    labelFontFamily: 'Barlow',
-    labelFontSize: 15,
-    labelFontWeight: 400,
-    labelFontVariant: 'normal',
-    labelOpacity: 1,
-  },
+  ...LayerProps,
 }
 
 const setupDebugger = (atlas, layers, element) => {
@@ -54,10 +25,6 @@ const setupDebugger = (atlas, layers, element) => {
   })
 
   const ctrlPan = ui.addFolder('Panning Controls')
-  const ctrlElevation = ui.addFolder('Elevation Layer')
-  const ctrlMarkers = ui.addFolder('Marker Layer')
-  const ctrlLabels = ui.addFolder('Labels Layer')
-  const ctrlColors = ui.addFolder('Color Bands')
 
   const layerPropSetter = (layer, prop) => {
     return (value) => {
@@ -81,68 +48,22 @@ const setupDebugger = (atlas, layers, element) => {
     .onChange(mapTransformSetter)
 
   // Setup Elevation Layer controls
-  ctrlElevation
-    .add(stateSet.elevation, 'visible', true)
-    .onChange(layerPropSetter(layers.elevation, 'visible'))
-  ctrlElevation
-    .add(stateSet.elevation, 'contourWidth', 0, 5, .1)
-    .onChange(layerPropSetter(layers.elevation, 'contourWidth'))
-  ctrlElevation
-    .add(stateSet.elevation, 'lightAltitude', 0, 6, .05)
-    .onChange(layerPropSetter(layers.elevation, 'lightAltitude'))
-  ctrlElevation
-    .add(stateSet.elevation, 'lightIntensity', 0, 5, .05)
-    .onChange(layerPropSetter(layers.elevation, 'lightIntensity'))
-  ctrlElevation
-    .add(stateSet.elevation, 'lightAzimuth', 0, 10, .1)
-    .onChange(layerPropSetter(layers.elevation, 'lightAzimuth'))
-  ctrlElevation
-    .add(stateSet.elevation, 'elevationPow', 0, 10, .1)
-    .onChange(layerPropSetter(layers.elevation, 'elevationPow'))
-  ctrlElevation
-    .add(stateSet.elevation, 'maxRadiusDivider', -10, 50, .1)
-    .onChange(layerPropSetter(layers.elevation, 'maxRadiusDivider'))
 
-  ctrlElevation
-    .add(stateSet.elevation, 'elevationOffset', -1, 1, .005)
-    .onChange(layerPropSetter(layers.elevation, 'elevationOffset'))
-  ctrlElevation
-    .add(stateSet.elevation, 'lightness', -1, 1, .001)
-    .onChange(layerPropSetter(layers.elevation, 'lightness'))
-  ctrlElevation
-    .add(stateSet.elevation, 'saturation', -1, 1, .001)
-    .onChange(layerPropSetter(layers.elevation, 'saturation'))
+  Object.entries(LayerPropConstraints)
+    .forEach(([layerName, props]) => {
+      const folder = ui.addFolder(`${layerName}`)
+      Object.entries(props).forEach(([property, constraint]) => {
+        folder
+          .add(stateSet[layerName], property, ...constraint)
+          .onChange(layerPropSetter(layers[layerName], property))
+      })
 
-  // Marker Layer Controls
-  ctrlMarkers
-    .add(stateSet.marker, 'visible')
-    .onChange(layerPropSetter(layers.markers, 'visible'))
-  ctrlMarkers
-    .add(stateSet.marker, 'markerSizeMultiplier', -1, 20, .5)
-    .onChange(layerPropSetter(layers.markers, 'markerSizeMultiplier'))
-  ctrlMarkers
-    .add(stateSet.marker, 'markerStrokeWidth', 0, 20, .5)
-    .onChange(layerPropSetter(layers.markers, 'markerStrokeWidth'))
-  ctrlMarkers
-    .add(stateSet.marker, 'markerFillOpacity', 0, 1, .05)
-    .onChange(layerPropSetter(layers.markers, 'markerFillOpacity'))
-  ctrlMarkers
-    .add(stateSet.marker, 'markerStrokeOpacity', 0, 1, .05)
-    .onChange(layerPropSetter(layers.markers, 'markerStrokeOpacity'))
-  ctrlMarkers
-    .add(stateSet.marker, 'minAbsoluteMarkerSize', 0, 20, .5)
-    .onChange(layerPropSetter(layers.markers, 'minAbsoluteMarkerSize'))
-  ctrlMarkers
-    .add(stateSet.marker, 'pointHoverRadiusMultiplier', 0, 50, 1)
-    .onChange(layerPropSetter(layers.markers, 'pointHoverRadiusMultiplier'))
-
-  // Labels Layer
-  ctrlLabels
-    .add(stateSet.labels, 'visible')
-    .onChange(layerPropSetter(layers.labels, 'visible'))
-  ctrlLabels
-    .add(stateSet.labels, 'labelFontSize', 8, 32, 1)
-    .onChange(layerPropSetter(layers.labels, 'labelFontSize'))
+      if (layerName === 'labels') {
+        folder
+          .addColor(stateSet[layerName], 'labelShadowColor')
+          .onChange(layerPropSetter(layers[layerName], 'labelShadowColor'))
+      }
+    })
 
   ui.close()
   ui.hide()
