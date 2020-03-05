7c17ca46bfc67e6a15b1b788ef27339a171c4989
Prashant Sinha <prashant+git@noop.pw>
2020-03-05 16:53:54 +0100

    Update setup signature

--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -3,22 +3,10 @@ import { setupInstance, renderView } from '~views/discover'
 
 import './style.scss'
 
-// Alrighty! devicePixelRatio is explosive. It needs to be a nice round integer.
-
-const pixelRatioClamped = () => {
-  const ratio = window.devicePixelRatio || 1
-  if (ratio < 1) {
-    // Really, lets not bother.
-    return 1
-  }
-  return Math.ceil(ratio)
-}
-
 window.addEventListener('load', async () => {
   await setup()
   setupInstance({
     element: document.getElementById('atlas'),
-    pixelRatio: pixelRatioClamped(),
   })
 
   renderView()
