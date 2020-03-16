e80f315bc8362dfa23e14340e8ddbcc7acb1fa96
Prashant Sinha <prashant+git@noop.pw>
2020-03-16 11:05:00 +0100

    Clean up the placeholder cards

--- src/components/resources/placeholders.js
+++ src/components/resources/placeholders.js
@@ -3,29 +3,23 @@ import { i18n } from '@ilearn/modules/i18n'
 
 const i18nT = i18n.context('pages.dashboard.resourceLists.states')
 
-export const NoMatches = () => {
-  return (
-    <div className='grid error no-match'>
-      <h3>{i18nT('noMatch.title')}</h3>
-      <p>{i18nT('noMatch.description')}</p>
-    </div>
-  )
-}
+export const NoMatches = () => (
+  <div className='grid error no-match'>
+    <h3>{i18nT('noMatch.title')}</h3>
+    <p>{i18nT('noMatch.description')}</p>
+  </div>
+)
 
-export const NoContent = () => {
-  return <>
-    <div className='grid error empty'>
-      <h3>{i18nT('empty.title')}</h3>
-      <p>{i18nT('empty.description')}</p>
-    </div>
-  </>
-}
+export const NoContent = () => (
+  <div className='grid error empty'>
+    <h3>{i18nT('empty.title')}</h3>
+    <p>{i18nT('empty.description')}</p>
+  </div>
+)
 
-export const ErrorDescription = (props) => {
-  return (
-    <div className='resources error server'>
-      <h3>{i18nT('error.title')}</h3>
-      <p>{i18nT('error.description')}</p>
-    </div>
-  )
-}
+export const ErrorDescription = () => (
+  <div className='resources error server'>
+    <h3>{i18nT('error.title')}</h3>
+    <p>{i18nT('error.description')}</p>
+  </div>
+)
