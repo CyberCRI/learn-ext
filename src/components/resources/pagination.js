de9896cf86c8a0b672b5f8f5b99d68c8ff008d2e
Prashant Sinha <prashant+git@noop.pw>
2020-04-06 19:03:38 +0200

    refactor: pagination is now a functional component

--- src/components/resources/pagination.js
+++ src/components/resources/pagination.js
@@ -1,12 +1,64 @@
-import React, { Component } from 'react'
-import { Button, Intent } from '@blueprintjs/core'
+import React from 'react'
+import { Button, ButtonGroup } from '@blueprintjs/core'
 
-const CELL_COUNT = 7
-const CELL_MID_LEN = ~~(CELL_COUNT / 2)
+const CELL_COUNT = 8
 
+function pagingCells (n, pos, maxCells=CELL_COUNT) {
+  // Consider an array of pages with length `n`. Let `p` be cursor position.
+  //  [1, 2, 3, ..., n-1, n]
+  //
+  // Requirements:
+  // - In all cases we want to keep `1` and `n` visible.
+  // - We cant render more than CELL_COUNT items.
+  // - If the cells exceed CELL_COUNT, insert `...` wherever appropriate.
+  const offset = n - pos
+  const pivot = ~~(maxCells / 2)
 
-class Pagination extends Component {
-  // This component implements pagination buttons such as these:
+  let cells = []
+
+  if (n > CELL_COUNT) {
+    // Fill in first and last positions
+    cells[0] = { nr: 1 }
+    cells[1] = { nr: 2 }
+    cells[CELL_COUNT - 2] = { nr: n - 1 }
+    cells[CELL_COUNT - 1] = { nr: n }
+
+    if (pos <= pivot) {
+      // last ellipse is enabled and the rest of the array is filled
+      cells[CELL_COUNT - 2].ellipsis = true
+      for (let i = 2; i < CELL_COUNT - 2; i++) {
+        cells[i] = { nr: i + 1 }
+      }
+    } else if (offset < pivot) {
+      // a ellipsis is enabled and the later part of array is filled
+      cells[1].ellipsis = true
+      for (let i = 2; i < CELL_COUNT - 2; i++) {
+        cells[i] = { nr: n - CELL_COUNT + i + 1 }
+      }
+    } else {
+      // both a and b ellipsis are enabled
+      cells[1].ellipsis = true
+      cells[CELL_COUNT - 2].ellipsis = true
+
+      // Current selected is put in centre
+      cells[pivot] = { nr: pos }
+      // Fill next and prev to mid point
+      // CELL_COUNT - 5 := n{MID, FIRST, SECOND, LAST, SECONDLAST}
+      for (let i = 1; i < CELL_COUNT - 5; i++) {
+        cells[pivot + i] = { nr: pos + i }
+        cells[pivot - i] = { nr: pos - i }
+      }
+    }
+  } else {
+    for (let i = 0; i < n; i++) {
+      cells[i] = { nr: i + 1, ellipsis: false }
+    }
+  }
+  return cells
+}
+
+const Pagination = ({count, cursor, onPaginate, maxCells=CELL_COUNT}) => {
+  // Renders a Pagination Button Group, inserting ellipsis based on cursor.
   // ┌───┬───┬───┬───┬───┐
   // │ < │ 1 │ 2 │ 3 │ > │
   // └───┴───┴───┴───┴───┘
@@ -16,112 +68,35 @@ class Pagination extends Component {
   // ┌───┬───┬───┬───┬───┬───┬───┬───┬───┐
   // │ < │ 1 │...│ 4 │ 5 │ 6 │...│10 │ > │
   // └───┴───┴───┴───┴───┴───┴───┴───┴───┘
-  //
-  // To implement the logic, consider a n page count in an Array struct.
-  // => [1, 2, 3, ..., n-1, n]
-  // Let p be current page.
-  // We have two possible ellipsis positions at 2 (a), and n-1 (b). We need to
-  // find if either of these ellipsis positions should be shown as ellipsis or
-  // as numbers.
-  // We see that if n <= CELL_COUNT, {a, b} := {false, false}
-  // In other cases, if p < (CELL_COUNT / 2), {a, b} := {false, true}
-  // and if p > (CELL_COUNT / 2), {a, b} := {true, false}.
 
-  constructor (props) {
-    super(props)
-    this.state = {
-      totalCount: props.totalCount || 0,
-      current: props.current || 1,
-    }
+  const PrevPage = <Button
+    icon='arrow-left'
+    disabled={cursor <= 1}
+    onClick={() => onPaginate(cursor - 1)}
+    text='Previous'/>
 
-    this.paginate = (nr) => props.onPaginate(nr)
-  }
+  const NextPage = <Button
+    rightIcon='arrow-right'
+    disabled={cursor >= count}
+    onClick={() => onPaginate(cursor + 1)}
+    text='Next'/>
 
-  componentWillReceiveProps (props) {
-    this.state = {
-      totalCount: props.totalCount,
-      current: props.current || 1,
-    }
-  }
-
-  getPagingLayout () {
-    const totalCount = this.state.totalCount, current = this.state.current
-    let pages = []
-
-    if (totalCount > CELL_COUNT) {
-      // Fill in first and last positions
-      pages[0] = { nr: 1 }
-      pages[1] = { nr: 2 }
-      pages[CELL_COUNT - 2] = { nr: totalCount - 1 }
-      pages[CELL_COUNT - 1] = { nr: totalCount }
-
-      if (current <= CELL_MID_LEN) {
-        // b ellipse is enabled and the rest of the array is filled
-        pages[CELL_COUNT - 2].ellipsis = true
-        for (let i = 2; i < CELL_COUNT - 2; i++) {
-          pages[i] = { nr: i + 1 }
-        }
-      } else if ((totalCount - current) < CELL_MID_LEN) {
-        // a ellipsis is enabled and the later part of array is filled
-        pages[1].ellipsis = true
-        for (let i = 2; i < CELL_COUNT - 2; i++) {
-          pages[i] = { nr: totalCount - CELL_COUNT + i + 1 }
-        }
-      } else {
-        // both a and b ellipsis are enabled
-        pages[1].ellipsis = true
-        pages[CELL_COUNT - 2].ellipsis = true
-
-        // Current selected is put in centre
-        pages[CELL_MID_LEN] = { nr: current }
-        // Fill next and prev to mid point
-        // CELL_COUNT - 5 := n{MID, FIRST, SECOND, LAST, SECONDLAST}
-        for (let i = 1; i < CELL_COUNT - 5; i++) {
-          pages[CELL_MID_LEN + i] = { nr: current + i }
-          pages[CELL_MID_LEN - i] = { nr: current - i }
-        }
-      }
-    } else {
-      for (let i = 0; i < totalCount; i++) {
-        pages[i] = { nr: i + 1, ellipsis: false }
-      }
-    }
-
-    pages.forEach(p => {
-      if (p.nr === this.state.current) {
-        p.active = true
-      }
-    })
-
-    return pages
-  }
-
-  render () {
-    const ltEnable = this.state.current > 1
-    const rtEnable = this.state.current < this.state.totalCount
-    const pages = this.getPagingLayout()
-
-    return (
-      <div className='pt-button-group'>
+  return (
+    <ButtonGroup>
+      {PrevPage}
+      {pagingCells(count, cursor, maxCells).map(({ nr, ellipsis }) =>
         <Button
-          icon='chevron-left'
-          disabled={!ltEnable}
-          onClick={() => this.paginate(this.state.current - 1)}/>
-        {pages.map(p =>
-          <Button
-            text={p.ellipsis ? '...' : p.nr}
-            key={p.nr}
-            disabled={p.ellipsis}
-            intent={p.active ? Intent.PRIMARY : Intent.DEFAULT}
-            onClick={() => this.paginate(p.nr)}/>
-        )}
-        <Button
-          icon='chevron-right'
-          disabled={!rtEnable}
-          onClick={() => this.paginate(this.state.current + 1)}/>
-      </div>
-    )
-  }
+          text={!ellipsis && nr}
+          icon={ellipsis && 'more'}
+          disabled={ellipsis}
+          key={nr}
+          active={nr === cursor}
+          outline
+          onClick={() => onPaginate(nr)}/>
+      )}
+      {NextPage}
+    </ButtonGroup>
+  )
 }
 
 export default Pagination
