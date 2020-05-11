575ac491594ac8a3e9885473f82309e154e19617
Prashant Sinha <prashant+git@noop.pw>
2020-05-11 13:41:10 +0200

    add grid styless for dpad in dpad-like way

--- src/views/discover/dpad-zoom.js
+++ src/views/discover/dpad-zoom.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Button } from '@blueprintjs/core'
 
 const DPadBox = styled.div`
   position: absolute;
   bottom: 0;
   right: 0;
+  display: grid;
+
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 2fr 2fr 1fr 1fr 1fr;
+  gap: 0 0;
+  grid-template-areas:
+    ".  zp  ."
+    ".  zm  ."
+    ".  up  ."
+    "lt .   rt"
+    ".  dn  .";
 `
-const DirectionalButton = styled.button``
+
+const Nudge = {
+  left: styled.div`grid-area: lt;`,
+  right: styled.div`grid-area: rt;`,
+  up: styled.div`grid-area: up;`,
+  down: styled.div`grid-area: dn;`,
+  zoomIn: styled.div`grid-area: zp;`,
+  zoomOut: styled.div`grid-area: zm;`,
+}
+
 
 export default function DPadButtons(props) {
   const didClick = (axis, delta) => () => {
@@ -14,11 +35,41 @@ export default function DPadButtons(props) {
   }
 
   return <DPadBox>
-    <DirectionalButton onClick={didClick('x', -1)}>Left</DirectionalButton>
-    <DirectionalButton onClick={didClick('x', 1)}>Right</DirectionalButton>
-    <DirectionalButton onClick={didClick('y', -1)}>Up</DirectionalButton>
-    <DirectionalButton onClick={didClick('y', 1)}>Down</DirectionalButton>
-    <DirectionalButton onClick={didClick('zoom', 1)}>Zoom In</DirectionalButton>
-    <DirectionalButton onClick={didClick('zoom', -1)}>Zoom Out</DirectionalButton>
+    <Nudge.right>
+      <Button
+        onClick={didClick('x', -1)}
+        outlined
+        icon='chevron-right'/>
+    </Nudge.right>
+    <Nudge.left>
+      <Button
+        onClick={didClick('x', 1)}
+        outlined
+        icon='chevron-left'/>
+    </Nudge.left>
+    <Nudge.up>
+      <Button
+        onClick={didClick('y', -1)}
+        outlined
+        icon='chevron-up'/>
+    </Nudge.up>
+    <Nudge.down>
+      <Button
+        onClick={didClick('y', 1)}
+        outlined
+        icon='chevron-down'/>
+    </Nudge.down>
+    <Nudge.zoomIn>
+      <Button
+        onClick={didClick('zoom', 1)}
+        outlined
+        icon='plus'/>
+    </Nudge.zoomIn>
+    <Nudge.zoomOut>
+      <Button
+        onClick={didClick('zoom', -1)}
+        outlined
+        icon='minus'/>
+    </Nudge.zoomOut>
   </DPadBox>
 }
