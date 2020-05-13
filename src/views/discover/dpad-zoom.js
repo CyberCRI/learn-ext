36c8f0a6101367b4265a5a3a68835deaa2f7183a
Prashant Sinha <prashant+git@noop.pw>
2020-05-13 20:08:38 +0200

    refactor dpad-zoom component

--- src/views/discover/dpad-zoom.js
+++ src/views/discover/dpad-zoom.js
@@ -1,76 +1,42 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Button } from '@blueprintjs/core'
+import { Button, ButtonGroup } from '@blueprintjs/core'
 
-const DPadBox = styled.div`
+const DPad = styled.div`
   position: absolute;
   bottom: 0;
   right: 0;
-  display: grid;
-
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 5px 1fr 1fr 1fr;
-  gap: 0 0;
-  grid-template-areas:
-    ".  zp  ."
-    ".  zm  ."
-    ".  .   ."
-    ".  up  ."
-    "lt .   rt"
-    ".  dn  .";
+  padding: 10px 20px;
 `
 
-const Nudge = {
-  left: styled.div`grid-area: lt;`,
-  right: styled.div`grid-area: rt;`,
-  up: styled.div`grid-area: up;`,
-  down: styled.div`grid-area: dn;`,
-  zoomIn: styled.div`grid-area: zp;`,
-  zoomOut: styled.div`grid-area: zm;`,
+export const useMapTransforms = () => {
+  const mapt = () => window._magic_atlas.mapt
+  return {
+    zoom: {
+      in:  () => mapt().zoom += 1,
+      out: () => mapt().zoom -= 1,
+    },
+    nudge: {
+      left:   () => mapt().x += 1,
+      right:  () => mapt().x -= 1,
+      down:   () => mapt().y += 1,
+      up:     () => mapt().y -= 1,
+    },
+  }
 }
 
 
-export default function DPadButtons(props) {
-  const didClick = (axis, delta) => () => {
-    window._magic_atlas.mapt[axis] += delta
-  }
 
-  return <DPadBox>
-    <Nudge.right>
-      <Button
-        onClick={didClick('x', -1)}
-        outlined
-        icon='chevron-right'/>
-    </Nudge.right>
-    <Nudge.left>
-      <Button
-        onClick={didClick('x', 1)}
-        outlined
-        icon='chevron-left'/>
-    </Nudge.left>
-    <Nudge.up>
-      <Button
-        onClick={didClick('y', -1)}
-        outlined
-        icon='chevron-up'/>
-    </Nudge.up>
-    <Nudge.down>
-      <Button
-        onClick={didClick('y', 1)}
-        outlined
-        icon='chevron-down'/>
-    </Nudge.down>
-    <Nudge.zoomIn>
-      <Button
-        onClick={didClick('zoom', 1)}
-        outlined
-        icon='plus'/>
-    </Nudge.zoomIn>
-    <Nudge.zoomOut>
-      <Button
-        onClick={didClick('zoom', -1)}
-        outlined
-        icon='minus'/>
-    </Nudge.zoomOut>
-  </DPadBox>
+export const ZoomButtons = (props) => {
+  const mapt = useMapTransforms()
+  return <ButtonGroup vertical>
+    <Button icon='plus' onClick={mapt.zoom.in}/>
+    <Button icon='minus' onClick={mapt.zoom.out}/>
+  </ButtonGroup>
+}
+
+export default function DPadButtons (props) {
+  return <DPad>
+    <ZoomButtons/>
+  </DPad>
 }
