40f26b6a0e3fd9360bded11135512b66bcd4d3f5
Prashant S <prashant@noop.pw>
2020-07-13 18:09:54 +0200

    override search-ui-view components so that we use our react components

--- src/views/discover/search-ui.js
+++ src/views/discover/search-ui.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import {
   SearchProvider, SearchBox,
-  Results, ResultsPerPage,
-  PagingInfo, Paging,
-  Sorting, Facet,
-  WithSearch,
-  ErrorBoundary,
+  PagingInfo, Paging, WithSearch, ErrorBoundary,
 } from '@elastic/react-search-ui'
-import { Layout } from '@elastic/react-search-ui-views'
 
-import '@elastic/react-search-ui-views/lib/styles/styles.css'
-
-import { getTagRepresentation } from '~components/concepts/concepts'
+import { ControlGroup, InputGroup, Button } from '@blueprintjs/core'
+import { ResourceGrid, Pagination } from '~components/resources'
+import { ConceptTag } from '~components/concepts'
 import { CarteSearchAPI } from '@ilearn/modules/api'
+import { viewportEvent } from './store'
 
 
 const searchConfig = {
@@ -21,11 +17,7 @@ const searchConfig = {
     const skip = args.resultsPerPage * (args.current - 1)
     const r = await CarteSearchAPI.search({ q: searchTerm, skip, limit })
     const results = r.results.map((n) => {
-      return {
-        ...n,
-        id: { raw: n.resource_id },
-        title: { raw: n.title },
-      }
+      return { ...n, id: { raw: n.resource_id } }
     })
     return {
       results,
@@ -47,14 +39,7 @@ const searchConfig = {
   },
   onAutocomplete: async ({ searchTerm }) => {
     const items = await CarteSearchAPI.typeahead({ q: searchTerm })
-    const suggestions = items.results.map((n) => {
-      const { title, lang } = getTagRepresentation(n)
-      return {
-        id: { raw: n.wikidata_id },
-        title: { raw: title },
-        ...n,
-      }
-    })
+    const suggestions = items.results
 
     return {
       autocompletedResults: suggestions,
@@ -62,37 +47,59 @@ const searchConfig = {
   },
 
   onAutocompleteResultClick: async (args) => console.log(args),
-  debug: true,
+  debug: false,
 }
 
-// sideContent = {
-//             < div >
-//   <Sorting label='Sort By' sortOptions={[{ name: 'relevance', value: '', direction: '' }, { name: 'no relevance at all!', value: '', direction: '' }]} />
-//   <Facet field='concepts' label='nope' filterType='any' />
-//             </div >
-//           }
+const SearchInput = ({ getAutocomplete, getInputProps, getButtonProps, ...args }) => {
+  console.log(args.isOpen)
+  return <>
+    <ControlGroup>
+      <InputGroup {...getInputProps()} leftIcon='search'/>
+      <Button {...getButtonProps()} icon='arrow-right' loading={args.isLoading}/>
+    </ControlGroup>
+    {getAutocomplete()}
+  </>
+}
 
-const SearchComposition = ({ wasSearched, isLoading }) => {
+const AutocompleteResults = ({ autocompletedResults, getItemProps, ...props }) => {
+  return (
+    <div className="sui-search-box__autocomplete-container">
+      {autocompletedResults.map((result, i) => (
+        <div key={result.wikidata_id} onClick={(e) => {
+          props.setSearchTerm(result.wikidata_id)
+        }}>
+          <ConceptTag {...result}/>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const ResultView = (props) => {
+  if (props.results) {
+    return <ResourceGrid resources={props.results} />
+  }
+  return <p>no results yet...</p>
+}
+
+
+const SearchComposition = ({ wasSearched, isLoading, ...props }) => {
+  React.useEffect(() => {
+    return viewportEvent.click.watch((args) => { props.setSearchTerm(args.data.title) })
+  })
   return (
     <div className='search-root'>
       <ErrorBoundary>
-        <Layout
-          header={<SearchBox
-            autocompleteResults={{
-              sectionTitle: "Suggested Results",
-              titleField: "title",
-            }}
-            autocompleteSuggestions={{
-              sectionTitle: "Suggested Queries"
-            }}
-          />}
-          bodyHeader={<>
-            {isLoading && <p>I am loading now. okay?</p>}
-            {wasSearched && <PagingInfo />}
-          </>}
-          bodyContent={<Results titleField='title' />}
-          bodyFooter={<Paging />}
-        />
+        <SearchBox
+          autocompleteResults={true}
+          autocompleteView={AutocompleteResults}
+          inputView={SearchInput}/>
+        <>
+          {isLoading && <p>I am loading now. okay?</p>}
+          {wasSearched && !isLoading && <PagingInfo />}
+        </>
+        <ResultView results={props.results}/>
+        <Pagination cursor={props.current} count={props.totalPages} onPaginate={props.setCurrent}/>
       </ErrorBoundary>
     </div>
   )
@@ -101,7 +108,7 @@ const SearchComposition = ({ wasSearched, isLoading }) => {
 export const SearchView = (props) => {
   return (
     <SearchProvider config={searchConfig}>
-      <WithSearch mapContextToProps={({ wasSearched, isLoading }) => ({ wasSearched, isLoading })}>
+      <WithSearch mapContextToProps={ctx => ctx}>
         {(props) => <SearchComposition {...props}/>}
       </WithSearch>
     </SearchProvider>
