9ae0fb7416972ce5abe15ceb7a7c5d1b26606675
Prashant Sinha <prashant@noop.pw>
2020-08-24 15:48:33 +0200

    make a demo of sui capabilities, awaiting query updates

--- src/views/discover/search-ui.js
+++ src/views/discover/search-ui.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import {
   SearchProvider, SearchBox,
-  PagingInfo, WithSearch, ErrorBoundary, Facet,
+  PagingInfo, WithSearch, ErrorBoundary, Facet, ResultsPerPage,
+  Sorting, Paging,
 } from '@elastic/react-search-ui'
-import { MultiCheckboxFacet } from "@elastic/react-search-ui-views"
+import {
+  MultiCheckboxFacet, SingleSelectFacet,
+  SingleLinksFacet, BooleanFacet,
+} from '@elastic/react-search-ui-views'
 import styled from 'styled-components'
 import _ from 'lodash'
+
+import '@elastic/react-search-ui-views/lib/styles/styles.css'
 
 import { ControlGroup, InputGroup, Button, Portal } from '@blueprintjs/core'
 import { ResourceGrid, Pagination } from '~components/resources'
@@ -35,6 +41,71 @@ const searchConfig = {
       results,
       totalResults: r.pagination.count,
       totalPages: ~~(r.pagination.count / r.pagination.limit) + 1,
+      facets: {
+        portal: [
+          {
+            field: 'portal',
+            type: 'value',
+            data: [
+              { value: 'science', count: 123 },
+              { value: 'math', count: 34 },
+              { value: 'bio', count: 40 },
+            ]
+          }
+        ],
+        academic_discipline: [
+          {
+            field: 'academic_discipline',
+            type: 'value',
+            data: [
+              { value: 'CS', count: 45 },
+              { value: 'BioTech', count: 100 },
+              { value: 'SysBio', count: 50 },
+            ]
+          }
+        ],
+        user: [
+          {
+            field: 'user',
+            type: 'value',
+            data: [
+              { value: 'My Resources', count: 152 },
+              { value: 'Everything', count: 9405 },
+              { value: 'CRI Projects', count: 1234 },
+            ],
+          }
+        ],
+        n_items: [
+          {
+            field: 'n_items',
+            type: 'range',
+            data: [
+              { value: { to: 0, from: 10, name: '0-10' }, count: 50 },
+              { value: { to: 10, from: 100, name: '10-100' }, count: 10 },
+            ],
+          },
+        ],
+        n_users: [
+          {
+            field: 'n_users',
+            type: 'range',
+            data: [
+              { value: { to: 0, from: 10, name: '0-10' }, count: 50 },
+              { value: { to: 10, from: 100, name: '10-100' }, count: 10 },
+            ],
+          },
+        ],
+        n_tags: [
+          {
+            field: 'n_tags',
+            type: 'range',
+            data: [
+              { value: { to: 0, from: 10, name: '0-10' }, count: 50 },
+              { value: { to: 10, from: 100, name: '10-100' }, count: 10 },
+            ],
+          },
+        ],
+      },
     }
   },
   onAutocomplete: async ({ searchTerm }) => {
@@ -47,39 +118,23 @@ const searchConfig = {
   },
   searchQuery: {
     facets: {
-      portal: { type: 'any', size: 2 },
-      marker: { type: 'any', size: 2 },
-      user: { type: 'any', size: 2 },
+      portal: { type: 'any' },
+      marker: { type: 'any' },
+      user:   { type: 'any' },
+      academic_discipline: { type: 'any', filterable: true },
+      n_items: { type: 'range' },
+      n_users:  { type: 'range' },
+      n_tags:   { type: 'range' },
     },
     alwaysSearchOnInitialLoad: false,
   },
   initialState: {
     searchTerm: '',
-    facets: {
-      portal: [
-        {
-          field: 'portal',
-          type: 'value',
-          data: [
-            { value: 'boop', count: 10 }
-          ]
-        }
-      ],
-      layer_source: [
-        {
-          field: 'layer_source',
-          type: 'value',
-          data: [
-            { value: 'mine', count: 1 },
-            { value: 'all', count: 1 },
-            { value: 'group', count: 1 },
-          ],
-        }
-      ],
-    },
+    resultsPerPage: 20,
   },
 
   onAutocompleteResultClick: async (args) => console.log(args),
+  hasA11yNotifications: false,
   debug: env.is_dev,
 }
 
@@ -120,7 +175,7 @@ const AutocompleteItemDiv = styled.div`
 
 const SearchInput = ({ getAutocomplete, getInputProps, getButtonProps, ...args }) => {
   const containerel = document.getElementById(SearchInputMountPoint)
-  return <Portal container={containerel} className='search-portal'>
+  return <>
     <SearchInputContainer>
       <ControlGroup>
         <InputGroup {...getInputProps()} leftIcon='search' placeholder='Find Concepts'/>
@@ -128,7 +183,7 @@ const SearchInput = ({ getAutocomplete, getInputProps, getButtonProps, ...args }
       </ControlGroup>
       {getAutocomplete()}
     </SearchInputContainer>
-  </Portal>
+  </>
 }
 
 
@@ -185,7 +240,7 @@ const PlaceHolder = (props) => {
 
 const ResultView = ({ results, wasSearched }) => {
   if (results && results.length) {
-    return <ResourceGrid resources={results} />
+    return <ResourceGrid resources={results} onDelete={console.log}/>
   }
 
   if (!wasSearched) {
@@ -217,31 +272,51 @@ const SearchComposition = ({ wasSearched, isLoading, ...props }) => {
     <div className='search-root'>
       <ErrorBoundary>
         <div className='top'>
-          {false &&
-            <SearchBox
-              autocompleteResults={true}
-              autocompleteView={AutocompleteResults}
-              onSelectAutocomplete={(item) => {
-                props.setFilter('source', 'marker')
-                props.setSearchTerm(item.wikidata_id, { shouldClearFilters: false })
-              }}
-              inputView={SearchInput}/>
-          }
-          <>
-            {isLoading && <p>Searching...</p>}
-            {wasSearched && !isLoading && <PagingInfo />}
-          </>
+          <SearchBox
+            autocompleteResults={true}
+            autocompleteView={AutocompleteResults}
+            onSelectAutocomplete={(item) => {
+              props.setFilter('source', 'marker')
+              props.setSearchTerm(item.wikidata_id, { shouldClearFilters: false })
+            }}
+            inputView={SearchInput}/>
         </div>
-        {false &&
-          <div>
-            <Facet field='portal' label='Portals' view={MultiCheckboxFacet}/>
-            <Facet field='layer_source' label='Map' view={MultiCheckboxFacet}/>
-          </div>}
         <div className='results'>
-          <ResultView results={props.results} wasSearched={wasSearched} loading={isLoading}/>
-        </div>
-        <div className='bottom'>
-          <Pagination cursor={props.current} count={props.totalPages} onPaginate={props.setCurrent}/>
+          <div className='tools'>
+            <div>
+              <Sorting
+                label={"Sort by"}
+                sortOptions={[
+                  {
+                    name: "Relevance",
+                    value: "",
+                    direction: ""
+                  },
+                  {
+                    name: "Recently Added",
+                    value: "new",
+                    direction: "dsc"
+                  }
+                ]}
+              />
+              <ResultsPerPage options={[10, 20, 30]} />
+            </div>
+            <div>
+              <Facet field='portal' label='Portals' view={MultiCheckboxFacet} />
+              <Facet field='user' label='Map' view={SingleSelectFacet} />
+              <Facet field='n_users' label='in nUsers Library' view={SingleLinksFacet} />
+              <Facet field='n_tags' label='Has n-tags' view={SingleLinksFacet} />
+              <Facet field='academic_discipline' label='academic_discipline' view={MultiCheckboxFacet} />
+            </div>
+          </div>
+          <div className='items'>
+            <div className='info-bar'>
+              {isLoading && <span>Searching... </span>}
+              {wasSearched && <PagingInfo />}
+            </div>
+            <ResultView results={props.results} wasSearched={wasSearched} loading={isLoading}/>
+            {wasSearched && <Paging />}
+          </div>
         </div>
       </ErrorBoundary>
     </div>
