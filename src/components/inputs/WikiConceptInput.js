7e64686e23844ee7ca77d2759a8a5ca6d102baa6
Prashant Sinha <prashant@noop.pw>
2020-11-20 15:58:55 +0100

    Move concepts/suggest into inputs/WikiConceptInput

--- src/components/inputs/WikiConceptInput.js
+++ src/components/inputs/WikiConceptInput.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
+import _ from 'lodash'
 import { useDebounce, useBoolean } from 'react-use'
-import { Tag, NonIdealState, Spinner } from '@blueprintjs/core'
+import { NonIdealState, Spinner, Button } from '@blueprintjs/core'
 import { Select } from '@blueprintjs/select'
 import clsx from 'classnames'
 
 import Wiki from '@ilearn/modules/api/wikipedia'
 import { i18n } from '@ilearn/modules/i18n'
+
+import { ConceptList } from '~components/concepts'
 
 const i18nT = i18n.context('components.suggest')
 
@@ -70,15 +73,15 @@ const ZeroResultsState = ({ loading, query }) => {
 const controlProps = {
   input: {
     placeholder: i18nT('searchInput.placeholder'),
-    className: 'suggest input',
+    className: 'wiki-concept suggest input',
     minimal: true,
   },
   popover: {
     position: 'bottom-left',
     minimal: false,
     usePortal: true,
-    className: 'suggest target',
-    portalClassName: 'suggest popover',
+    className: 'wiki-concept suggest target',
+    portalClassName: 'wiki-concept suggest popover',
     modifiers: {
       arrow: { enabled: true },
       flip: { enabled: true },
@@ -86,7 +89,7 @@ const controlProps = {
   },
 }
 
-export const ConceptSuggest = ({ onSelect, lang, usePortal=true }) => {
+export const WikiConceptSuggest = ({ onSelect, lang, usePortal=true }) => {
   const [ query, setQuery ] = useState('')
   const [ items, setItems ] = useState([])
   const [ loading, setLoadingState ] = useBoolean(false)
@@ -114,7 +117,7 @@ export const ConceptSuggest = ({ onSelect, lang, usePortal=true }) => {
   }, 300, [ query ])
 
   return (
-    <div className='suggest root'>
+    <div className='wiki-concept suggest root'>
       <Select
         resetOnQuery={false}
         resetOnSelect={false}
@@ -133,10 +136,32 @@ export const ConceptSuggest = ({ onSelect, lang, usePortal=true }) => {
         query={query}
         onItemSelect={didSelectItem}
         onQueryChange={(q) => setQuery(q)}>
-        <Tag interactive large minimal intent='primary'>
-          {i18nT('searchInput.placeholder')}
-        </Tag>
+        <Button icon='plus' intent='primary' outlined minimal text={i18nT('searchInput.placeholder')}/>
       </Select>
+    </div>
+  )
+}
+
+export const WikiConceptInput = ({ value, lang, onChange, usePortal=true }) => {
+  const [ concepts, setConcepts ] = useState(value || [])
+
+  React.useEffect(() => {
+    onChange(concepts)
+  }, [concepts])
+
+  const didAddConcept = (concept) => {
+    const newSelection = _.unionBy(concepts, [concept], 'wikidata_id')
+    setConcepts(newSelection)
+  }
+  const didRemoveConcept = (concept) => {
+    const newSelection = _.reject(concepts, ['wikidata_id', concept.wikidata_id])
+    setConcepts(newSelection)
+  }
+
+  return (
+    <div className='wiki-concept input'>
+      <ConceptList concepts={concepts} removable onRemove={didRemoveConcept}/>
+      <WikiConceptSuggest onSelect={didAddConcept} lang={lang} usePortal={usePortal}/>
     </div>
   )
 }
