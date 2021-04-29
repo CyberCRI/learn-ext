07fa194b7ac03dfd0b577cc1c648c32f4e2b01e4
Prashant Sinha <prashant@noop.pw>
2021-04-29 13:28:33 +0200

    Implement Resource Details view

---
+++ src/components/resources/details-view.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { useStore } from 'effector-react'
+import { useAsync } from 'react-use'
+import styled from 'styled-components'
+
+import { Dialog, NonIdealState } from '@blueprintjs/core'
+
+import { ResourceCard } from '~components/cards/resources'
+import { ResourceItem } from '~components/resources'
+
+import { RecommendedResourcesAPI } from '@ilearn/modules/api'
+
+import { $DetailsDialog, ResourceDetailsDialogControl } from './store'
+
+
+const SimilarResourcesListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: start;
+
+  .resource.card {
+    margin: 10px;
+  }
+`
+
+const SimilarResourcesList = ({ resources }) => {
+  if (resources.length === 0) {
+    return <NonIdealState
+      icon='offline'
+      description={'No Similar Resources found.'}/>
+  }
+  return <SimilarResourcesListContainer>
+    {resources.map(r => <ResourceCard {...r}/>)}
+  </SimilarResourcesListContainer>
+}
+
+const SimilarResources = ({ resource_id, ...props }) => {
+  const resources = useAsync(async () => {
+    return await RecommendedResourcesAPI.getSimilarResources({ resource_id })
+  }, [resource_id])
+
+  return <div>
+    {resources.loading && <p>Loading</p>}
+    {resources.value && <SimilarResourcesList resources={resources.value}/>}
+  </div>
+}
+
+
+export const ResourceDetailsDialog = () => {
+  const dialog = useStore($DetailsDialog)
+  const resource = dialog.resource
+
+  return (
+    <Dialog
+      isOpen={dialog.isOpen}
+      onClose={ResourceDetailsDialogControl.hide}
+      className='dialog resource-details-view'
+      title={resource ? resource.title : ''}
+      icon='intersection'>
+      <div className='body'>
+        {resource && <ResourceItem {...resource}/>}
+        <h3 className='heading'>Similar Resources</h3>
+        {resource && <SimilarResources resource_id={resource.resource_id}/>}
+      </div>
+    </Dialog>
+  )
+}
