e3c7fb0141bf77e070654e9ef4bf41ad3b73a213
Prashant Sinha <prashant@noop.pw>
2021-05-19 15:02:22 +0200

    Export ResourceEditDialog

--- src/components/resources/index.js
+++ src/components/resources/index.js
@@ -4,5 +4,6 @@ import * as Placeholder from './placeholders'
 import { Pagination } from './pagination'
 import { ResourceGrid } from './grid'
 import { ResourceListView, ResourceItem } from './resource-view'
+import { ResourceEditDialog } from './edit-resource'
 
-export { Placeholder, Pagination, ResourceGrid, ResourceListView, ResourceItem }
+export { Placeholder, Pagination, ResourceGrid, ResourceListView, ResourceItem, ResourceEditDialog }
